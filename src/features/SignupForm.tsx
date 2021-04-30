6259f85bbbd149244a91c979d0561857827f92f0
iacopolea <iacopo.leardini@app-quality.com>
2021-04-30 17:35:54 +0200

    fix: add checkbox required in signup form validation

--- src/features/SignupForm.tsx
+++ src/features/SignupForm.tsx
@@ -21,7 +21,7 @@ export const SignupForm = () => {
     surname: yup.string().required(),
     email: yup.string().email().required(),
     password: yup.string().min(8, 'password must be at least 8 character').required(),
-    subscribe: yup.boolean().oneOf([true], 'Must Accept Terms and Conditions')
+    subscribe: yup.boolean().oneOf([true], 'Must Accept Terms and Conditions').required()
   }
   return (
     <Formik
