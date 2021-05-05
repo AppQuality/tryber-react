e2e6e578caf8d441b96161c48e48498c90a50b45
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-05 16:32:46 +0200

    chore: Fix form storybook

--- src/stories/form/Form.stories.tsx
+++ src/stories/form/Form.stories.tsx
@@ -1,13 +1,19 @@
-import React from "react";
 import { Field, FieldInterface } from "./Form";
 import {Story, Meta} from "@storybook/react";
+import { Formik, Form } from "formik";
 
 export default {
   title: "Forms",
   component: Field,
 } as Meta;
 
-const Template: Story<FieldInterface> = (args) => <Field {...args} />;
+const Template: Story<FieldInterface> = (args) => (
+  <Formik initialValues={{text:''}}  onSubmit={(data)=>{console.log(data)}}>
+    <Form>
+      <Field {...args} />
+    </Form>
+  </Formik>
+);
 
 export const Text = Template.bind({});
 Text.args = {
