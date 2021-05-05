5f3ce34ed4f32ae45153eb2977666220bbe40d1f
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-05 11:42:45 +0200

    test: Add basic tests for signupform

---
+++ src/features/SignupForm.spec.tsx
@@ -0,0 +1,125 @@
+import * as React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import userEvent from "@testing-library/user-event";
+import API from "../utils/api";
+import { mocked } from "ts-jest/utils";
+
+import { ThemeProvider } from "styled-components";
+import { aqBootstrapTheme } from "../stories/theme/defaultTheme";
+
+import { SignupForm } from "./SignupForm";
+import "../i18n";
+
+jest.mock("../utils/api");
+
+const mockedApi = mocked(API, true);
+
+test("SignupForm button should be disabled until all input are filled correctly", async () => {
+  const { debug } = render(
+    <ThemeProvider theme={aqBootstrapTheme}>
+      <SignupForm />
+    </ThemeProvider>
+  );
+  expect(screen.getByText("Create an account")).toBeInTheDocument();
+
+  expect(screen.getByLabelText("Name")).toHaveAttribute("type", "text");
+  expect(screen.getByLabelText("Surname")).toHaveAttribute("type", "text");
+  expect(screen.getByLabelText("Email")).toHaveAttribute("type", "email");
+  expect(screen.getByLabelText("Password")).toHaveAttribute("type", "password");
+
+  expect(screen.getByRole("button")).toHaveAttribute("type", "submit");
+  expect(screen.getByRole("button")).toHaveAttribute("disabled");
+
+  const signupData = {
+    name: "Pippo",
+    surname: "Franco",
+    email: "pippo.franco@example.com",
+    password: "pippoFranco0",
+  };
+
+  userEvent.type(screen.getByLabelText("Name"), signupData.name);
+  await waitFor(() => {
+    expect(screen.getByLabelText("Name")).toHaveValue(signupData.name);
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+  });
+
+  userEvent.type(screen.getByLabelText("Surname"), signupData.surname);
+  await waitFor(() => {
+    expect(screen.getByLabelText("Surname")).toHaveValue(signupData.surname);
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+  });
+
+  userEvent.type(screen.getByLabelText("Email"), signupData.email);
+  await waitFor(() => {
+    expect(screen.getByLabelText("Email")).toHaveValue(signupData.email);
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+  });
+
+  userEvent.type(screen.getByLabelText("Password"), signupData.password);
+  await waitFor(() => {
+    expect(screen.getByLabelText("Password")).toHaveValue(signupData.password);
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+  });
+
+  userEvent.click(
+    screen.getByLabelText(
+      "I agree to receive earning opportunity emails from AppQuality"
+    )
+  );
+  await waitFor(() => {
+    expect(
+      screen.getByLabelText(
+        "I agree to receive earning opportunity emails from AppQuality"
+      )
+    ).toBeChecked();
+    expect(screen.getByRole("button")).not.toHaveAttribute("disabled");
+  });
+});
+
+test("SignupForm fields should show a validation error when not validating", async () => {
+  const { debug } = render(
+    <ThemeProvider theme={aqBootstrapTheme}>
+      <SignupForm />
+    </ThemeProvider>
+  );
+  expect(screen.getByText("Create an account")).toBeInTheDocument();
+
+	screen.getByLabelText("Name").focus()
+	screen.getByLabelText("Name").blur()
+  await waitFor(() => {
+		expect(screen.getByLabelText("Name").classList.contains('is-invalid')).toBe(true)
+  });
+
+	screen.getByLabelText("Surname").focus()
+	screen.getByLabelText("Surname").blur()
+  await waitFor(() => {
+		expect(screen.getByLabelText("Surname").classList.contains('is-invalid')).toBe(true)
+  });
+	
+	screen.getByLabelText("Email").focus()
+	screen.getByLabelText("Email").blur()
+  await waitFor(() => {
+		expect(screen.getByLabelText("Email").classList.contains('is-invalid')).toBe(true)
+  });
+	
+	screen.getByLabelText("Password").focus()
+	screen.getByLabelText("Password").blur()
+  await waitFor(() => {
+		expect(screen.getByLabelText("Password").classList.contains('is-invalid')).toBe(true)
+  });
+	
+  // const weakPasswords = ["pippoFranc0", "pippofranco", "pippofranco0", "pippoFranco"];
+	// 
+	// await weakPasswords.forEach(async(weakPassword) => {
+	// 	userEvent.type(screen.getByLabelText("Password"), weakPassword);
+	//   await waitFor(() => {
+	// 
+	//     expect(screen.getByLabelText("Password")).toHaveValue(weakPassword);
+	// 		expect(screen.getByLabelText("Password").classList.contains('is-invalid')).toBe(true)
+	//   });
+	// 
+	// 
+	// });
+});
