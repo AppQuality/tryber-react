228502b6daa5a1f814060eaf94705a9900f2ad4d
iacopolea <iacopo.leardini@app-quality.com>
2021-05-11 18:24:16 +0200

    feat: add table component init

--- src/stories/styled.d.ts
+++ src/stories/styled.d.ts
@@ -6,6 +6,7 @@ declare module 'styled-components' {
     typography: {
       fontFamily: {
         base: string
+        serif: string
       }
       fontSize: {
         base: string
@@ -20,6 +21,7 @@ declare module 'styled-components' {
       primary: string
       secondary: string
       disabledFont: string
+      disabledElement: string
     }
     grid: {
       breakpoints: {
