e0a8b7a37a47efe31c6c683fd62faf9b10b8273d
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-12 18:23:17 +0200

    fix: Restore app without sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { GettingStarted} from "./pages";
 import SignIn from "./pages/SignIn";
 import {Header} from "./stories/header/Header";
-import {Sidebar} from "./stories/sidebar/Sidebar";
 import './i18n';
 import TagManager from 'react-gtm-module';
-import { HouseFill, PersonFill, Laptop } from "react-bootstrap-icons";
-
+ 
 const tagManagerArgs = {
     gtmId: 'GTM-K55XC7S',
     dataLayerName: 'PageDataLayer'
@@ -19,40 +17,12 @@ const tagManagerArgs = {
 TagManager.initialize(tagManagerArgs)
 const base = '/:locale(en|it)?';
 
-const sidebarArgs =  {
-  open: true,
-  languages: {
-    current: {lang: 'it'},
-    others: [{lang:'en', onClick: (lang:string) => {alert(lang)}},{lang:'es'}]
-  },
-  items: [
-    {
-      url: "#",
-      icon: <HouseFill />,
-      active: true,
-      text: "Dashboard",
-    },
-    {
-      url: "#",
-      icon: <PersonFill />,
-      active: false,
-      text: "Profilo",
-    },
-    {
-      url: "#",
-      icon: <Laptop />,
-      active: false,
-      text: "Dispositivi",
-    },
-  ],
-}
 function App() {
-  
+
   return (
     <ThemeProvider theme={aqBootstrapTheme}>
       <BrowserRouter>
         <Header showLogin={false}/>
-        <Sidebar {...sidebarArgs} >
         <Switch>
           <Route path={`${base}/getting-started`} component={GettingStarted} />
           <Route path={`${base}/it/getting-started-2`}>
@@ -60,7 +30,6 @@ function App() {
           </Route>
           <Route path={`${base}/login`} component={SignIn} />
         </Switch>
-        </Sidebar>
       </BrowserRouter>
     </ThemeProvider>
   );
