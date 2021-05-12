bb06ea0ed0ce09fd81033c715cbd82a89f804b93
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-12 18:21:53 +0200

    feat: Rebuild sidebar on resize

--- src/stories/sidebar/Sidebar.tsx
+++ src/stories/sidebar/Sidebar.tsx
@@ -1,5 +1,19 @@
 import { DesktopSidebar } from "./DesktopSidebar";
 import { SidebarProps } from "./SidebarProps";
+import { useLayoutEffect, useState } from 'react';
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 
 export const Sidebar = ({
   children,
@@ -7,6 +21,10 @@ export const Sidebar = ({
   languages,
   open = false,
 }: SidebarProps) => {
+  useWindowSize();
+  if (window.matchMedia('only screen and (max-width: 768px)').matches) {
+    return <div>mobile</div>
+  }
   return (
     <DesktopSidebar items={items} languages={languages} open={open}>
       {children}
