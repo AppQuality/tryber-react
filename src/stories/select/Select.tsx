dd42fbad2d82942a9cd684cb7b2080101a221bbe
iacopolea <iacopo.leardini@app-quality.com>
2021-05-14 16:35:52 +0200

    feat: add custom style with react-select rules invented by Satana

--- src/stories/select/Select.tsx
+++ src/stories/select/Select.tsx
@@ -1,4 +1,4 @@
-import ReactSelect, {components, Theme} from "react-select"
+import ReactSelect, {components, Styles, Theme} from "react-select"
 import {aqBootstrapTheme} from "../theme/defaultTheme"
 import {Search} from "react-bootstrap-icons"
 
@@ -37,9 +37,28 @@ export const Select = ({options, isSearch}: SelectProps) => {
     rest.components = {...rest.components, DropdownIndicator};
   }
 
+  const customStyle: Styles<any, any> = {
+    control: (provided, state) => {
+      const borderColor = state.isFocused
+        ? `${aqBootstrapTheme.colors.grey600}`
+        : `${aqBootstrapTheme.colors.grey100}`;
+      const boxShadow = state.isFocused
+        ? `inset 0 1px 2px rgb(0 0 0 / 8%), 0 0 0 0.25rem rgb(23 64 92 / 25%)`
+        : `inset 0 1px 2px rgb(0 0 0 / 8%)`
+      return {
+        ...provided,
+        borderColor,
+        boxShadow,
+        ':hover': {
+          borderColor
+        },
+      };
+    }
+  }
   return (
     <ReactSelect
       options={options}
+      styles={customStyle}
       theme={theme => aqTheme(theme)}
       {...rest}
     />
