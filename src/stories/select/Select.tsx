a1bc4881d6d7c7335bf0a5e9c93bb9ba380a278a
iacopolea <iacopo.leardini@app-quality.com>
2021-05-17 12:04:43 +0200

    feat: init async/pagination logic in component select

--- src/stories/select/Select.tsx
+++ src/stories/select/Select.tsx
@@ -1,13 +1,24 @@
 import ReactSelect, {components, Styles, Theme} from "react-select"
 import {aqBootstrapTheme} from "../theme/defaultTheme"
 import {ChevronDown, X} from "react-bootstrap-icons"
+import {useEffect, useReducer, useState} from "react";
 
 interface Option {
-  value: string
   label: string
+  value?: string
+  options?: Option[]
+  isDisabled?: boolean
+  isFixed?: boolean
+  [index: string]: any;
 }
+
+export type GetOptionsAsync = () => Promise<Option[]>;
+// () => { github.get('repositories').then( rep => rep.map(r => {r.id,r.name}))
+// () => { api.get('/api/users/me/bugs').then( bugs => bug.map( b => b.campaign_name).unique())
+
+
 export interface SelectProps {
-  options: Option[]
+  options: Option[] | GetOptionsAsync
   defaultValue?: string
   placeholder?: string
   isMulti?: boolean
@@ -17,7 +28,49 @@ export interface SelectProps {
   isSearchable?: boolean
 }
 
+type OptionActionType = 'add' | 'reset' | 'set';
+interface OptionAction {
+  type: OptionActionType,
+  payload: Option[]
+}
+
 export const Select = ({options, defaultValue, placeholder = 'Select...', isMulti, isDisabled, isLoading, isClearable = true, isSearchable}: SelectProps) => {
+  const [loading, setLoading] = useState(isLoading);
+
+  function updateOptions (state: Option[], action: OptionAction): Option[] {
+    const {type, payload} = action;
+    switch (type) {
+      case "set": {
+        return payload;
+      }
+      case 'add': {
+        return [...state, ...payload];
+      }
+      case "reset": {
+        return [];
+      }
+    }
+  };
+
+  const [optionsArray, setOptions] = useReducer(updateOptions, []);
+
+  useEffect(()=>{
+    setLoading(isLoading);
+  }, [isLoading]);
+
+  useEffect(() => {
+    if (options instanceof Array) {
+      setOptions({type: 'set', payload: options});
+    } else {
+      getAsyncRes('set', options);
+    }
+  });
+
+  const getAsyncRes = async (type: OptionActionType, fn: GetOptionsAsync) => {
+    const res = await fn();
+    setOptions({type: type, payload: res});
+  }
+
   let aqTheme = (theme: Theme) => ({
     borderRadius: 5,
     spacing: theme.spacing,
@@ -48,6 +101,10 @@ export const Select = ({options, defaultValue, placeholder = 'Select...', isMult
   // @ts-ignore
   rest.components = {...rest.components, DropdownIndicator, ClearIndicator};
 
+  const onMenuScrollToBottom = () => {
+    setLoading(true);
+    // addOptions
+  }
   const customStyle: Styles<any, any> = {
     control: (provided, state) => {
       const borderColor = state.isFocused
@@ -68,15 +125,17 @@ export const Select = ({options, defaultValue, placeholder = 'Select...', isMult
   }
   return (
     <ReactSelect
-      options={options}
+      options={[...optionsArray, loading ? { value: 'loading', label: 'Loading more...', isDisabled: true } : {}]}
       defaultValue={defaultValue}
       placeholder={placeholder}
       isDisabled={isDisabled}
-      isLoading={isLoading}
+      isLoading={loading}
       isClearable={isClearable}
       isSearchable={isSearchable}
       styles={customStyle}
       isMulti={isMulti}
+      // menuPlacement='auto'
+      onMenuScrollToBottom={onMenuScrollToBottom}
       theme={theme => aqTheme(theme)}
       {...rest}
     />
