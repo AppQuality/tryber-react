a5b9b08091dc8ea52fda2b2cf6f1862ae5957c7d
iacopolea <iacopo.leardini@app-quality.com>
2021-05-17 12:05:07 +0200

    feat: add grouped options

--- src/stories/select/Select.stories.tsx
+++ src/stories/select/Select.stories.tsx
@@ -1,6 +1,6 @@
 import {BSCol, Container} from "../layout/Layout"
 import {Card} from "../card/Card"
-import {Select, SelectProps} from "./Select"
+import {GetOptionsAsync, Select, SelectProps} from "./Select"
 import {Story, Meta} from "@storybook/react"
 import {aqBootstrapTheme} from "../theme/defaultTheme";
 import {ThemeProvider} from "styled-components";
@@ -16,6 +16,34 @@ const options = [
   { value: 'vanilla', label: 'Vanilla' }
 ];
 
+const groupedOptions = [
+  { label: 'A',
+    options: [
+      {value: "ocean", label: "Ocean"},
+      {value: "blue", label: "Blue", isDisabled: true},
+      {value: "purple", label: "Purple"},
+      {value: "red", label: "Red", isFixed: true},
+      {value: "orange", label: "Orange"},
+      {value: "yellow", label: "Yellow"},
+      {value: "green", label: "Green"},
+      {value: "forest", label: "Forest"},
+      {value: "slate", label: "Slate"},
+      {value: "silver", label: "Silver"},
+   ]
+  },
+  {label: 'B',
+    options: [
+      {value: "vanilla", label: "Vanilla", rating: "safe"},
+      {value: "chocolate", label: "Chocolate", rating: "good"},
+      {value: "strawberry", label: "Strawberry", rating: "wild"},
+      {value: "salted-caramel", label: "Salted Caramel", rating: "safe"},
+   ]
+  }
+]
+
+// const getAsyncOptions: GetOptionsAsync = () => {
+//   return new Promise<Option[]>()
+// }
 
 const Template: Story<SelectProps> = (args) => {
   return (
@@ -33,6 +61,17 @@ const Template: Story<SelectProps> = (args) => {
 
 export const SelectBase = Template.bind({});
 SelectBase.args = {
-  options: options,
+  options: options
+};
+
+export const GroupedAsync = Template.bind({});
+GroupedAsync.args = {
+  options: groupedOptions,
   isSearchable: true
 };
+
+// export const SelectAsync = Template.bind({});
+// SelectAsync.args = {
+//   options: getAsyncOptions,
+//   isSearchable: true
+// }
