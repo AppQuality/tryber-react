93285675130a9e8610a3253651c83e94e027ebf5
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-19 16:14:23 +0200

    chore: Run prettier

--- src/stories/select/Select.spec.tsx
+++ src/stories/select/Select.spec.tsx
@@ -1,9 +1,9 @@
-import {render} from "@testing-library/react";
-import {ThemeProvider} from "styled-components";
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
 import React from "react";
-import {Select} from "./Select";
-import {aqBootstrapTheme} from "../theme/defaultTheme"
-import {basicOptions} from "./_data";
+import { Select } from "./Select";
+import { aqBootstrapTheme } from "../theme/defaultTheme";
+import { basicOptions } from "./_data";
 
 describe("Basic Select should render options correctly", () => {
   it("There should be as many options as provided", () => {
--- src/stories/select/Select.stories.tsx
+++ src/stories/select/Select.stories.tsx
@@ -1,11 +1,11 @@
-import {BSCol, Container} from "../layout/Layout"
-import {Card} from "../card/Card"
-import {Select} from "./Select"
-import {SelectProps} from "./_types"
-import {basicOptions, getAsyncOptions, groupedOptions} from "./_data"
-import {Story, Meta} from "@storybook/react"
-import {aqBootstrapTheme} from "../theme/defaultTheme";
-import {ThemeProvider} from "styled-components";
+import { BSCol, Container } from "../layout/Layout";
+import { Card } from "../card/Card";
+import { Select } from "./Select";
+import { SelectProps } from "./_types";
+import { basicOptions, getAsyncOptions, groupedOptions } from "./_data";
+import { Story, Meta } from "@storybook/react";
+import { aqBootstrapTheme } from "../theme/defaultTheme";
+import { ThemeProvider } from "styled-components";
 
 export default {
   title: "Select",
@@ -16,7 +16,7 @@ const Template: Story<SelectProps> = (args) => {
   return (
     <ThemeProvider theme={aqBootstrapTheme}>
       <Container>
-        <BSCol size='col-lg-9'>
+        <BSCol size="col-lg-9">
           <Card>
             <Select {...args} />
           </Card>
@@ -28,17 +28,17 @@ const Template: Story<SelectProps> = (args) => {
 
 export const SelectBase = Template.bind({});
 SelectBase.args = {
-  options: basicOptions
+  options: basicOptions,
 };
 
 export const GroupedAsync = Template.bind({});
 GroupedAsync.args = {
   options: groupedOptions,
-  isSearchable: true
+  isSearchable: true,
 };
 
 export const SelectAsync = Template.bind({});
 SelectAsync.args = {
   options: getAsyncOptions,
-  isSearchable: true
-}
+  isSearchable: true,
+};
