a21427a7bdf5b3e0c8306450fa35decbe166282f
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-19 11:37:54 +0200

    fix: Capture scroll on mobile

--- src/stories/select/Select.tsx
+++ src/stories/select/Select.tsx
@@ -1,15 +1,21 @@
-import ReactSelect from "react-select"
-import {useEffect, useReducer, useState} from "react"
-import {aqTheme, customComponents, customStyle } from "./_styles"
-import {Option, GetOptionsAsync, OptionAction, OptionActionType, SelectProps} from "./_types"
+import ReactSelect from "react-select";
+import { useEffect, useReducer, useState } from "react";
+import { aqTheme, customComponents, customStyle } from "./_styles";
+import {
+  Option,
+  GetOptionsAsync,
+  OptionAction,
+  OptionActionType,
+  SelectProps,
+} from "./_types";
 
-function updateOptions (state: Option[], action: OptionAction): Option[] {
-  const {type, payload} = action;
+function updateOptions(state: Option[], action: OptionAction): Option[] {
+  const { type, payload } = action;
   switch (type) {
     case "set": {
       return payload;
     }
-    case 'add': {
+    case "add": {
       return [...state, ...payload];
     }
     case "reset": {
@@ -18,55 +24,69 @@ function updateOptions (state: Option[], action: OptionAction): Option[] {
   }
 }
 
-export const Select = ({options, defaultValue, placeholder = 'Select...', isMulti, isDisabled, isLoading, isClearable = true, isSearchable}: SelectProps) => {
+export const Select = ({
+  options,
+  defaultValue,
+  placeholder = "Select...",
+  isMulti,
+  isDisabled,
+  isLoading,
+  isClearable = true,
+  isSearchable,
+}: SelectProps) => {
   const [loading, setLoading] = useState(isLoading);
   const [searching, setSearching] = useState<string | false>(false);
   const [page, setPage] = useState(0);
   const [thereIsMore, setMore] = useState(true);
   const [optionsArray, setOptions] = useReducer(updateOptions, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     setLoading(isLoading);
   }, [isLoading]);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (thereIsMore) {
-      triggerUpdate()
+      triggerUpdate();
     }
   }, [page]);
 
   useEffect(() => {
     if (options instanceof Array) {
-      setOptions({type: 'set', payload: options});
+      setOptions({ type: "set", payload: options });
     }
   }, []);
 
   const triggerUpdate = () => {
     if (options instanceof Function) {
       setLoading(true);
-      getAsyncRes('add', options, page).finally(() => {
+      getAsyncRes("add", options, page).finally(() => {
         setLoading(false);
-      })
+      });
     }
-  }
+  };
 
-  const getAsyncRes = async (type: OptionActionType, fn: GetOptionsAsync, start: number, search?: string) => {
+  const getAsyncRes = async (
+    type: OptionActionType,
+    fn: GetOptionsAsync,
+    start: number,
+    search?: string
+  ) => {
     const res = await fn(start, search);
     setMore(res.more);
-    setOptions({type: type, payload: res.results});
-  }
+    setOptions({ type: type, payload: res.results });
+  };
 
-  const handleInputChange = (value:string) => {
+  const handleInputChange = (value: string) => {
     if (options instanceof Function) {
       setSearching(value);
       if (value.length >= 2) {
         setLoading(true);
-        getAsyncRes('set', options, 0, value).finally(() => {
+        getAsyncRes("set", options, 0, value).finally(() => {
           setLoading(false);
         });
       }
     }
-  }
+  };
 
   const handleChange = () => {
     if (options instanceof Function) {
@@ -74,7 +94,7 @@ export const Select = ({options, defaultValue, placeholder = 'Select...', isMult
         resetOptions();
       }
     }
-  }
+  };
 
   const handleBlur = () => {
     if (searching) {
@@ -82,28 +102,40 @@ export const Select = ({options, defaultValue, placeholder = 'Select...', isMult
     }
     setSearching(false);
     setLoading(false);
-  }
+  };
 
   const resetOptions = () => {
-    setOptions({type: 'reset', payload: []});
+    setOptions({ type: "reset", payload: [] });
     setMore(true);
-    setPage(page => {
+    setPage((page) => {
       if (page === 0) {
         triggerUpdate();
       }
-      return 0
+      return 0;
     });
-  }
+  };
 
   const onMenuScrollToBottom = () => {
-    if (thereIsMore) setPage(page => page+1); // this is not the updated value of thereIsMore untill rerender :((
-  }
+    if (thereIsMore) setPage((page) => page + 1); // this is not the updated value of thereIsMore untill rerender :((
+  };
 
   const optionsDropdown = [...optionsArray];
   if (loading) {
-    optionsDropdown.push({ value: 'loading-placeholder', label: 'Loading more...', isDisabled: true })
-  } else if (searching && typeof searching === 'string' && searching.length < 2) {
-    optionsDropdown.push({ value: 'search-placeholder', label: 'Please write at list 2 characters to load more results', isDisabled: true })
+    optionsDropdown.push({
+      value: "loading-placeholder",
+      label: "Loading more...",
+      isDisabled: true,
+    });
+  } else if (
+    searching &&
+    typeof searching === "string" &&
+    searching.length < 2
+  ) {
+    optionsDropdown.push({
+      value: "search-placeholder",
+      label: "Please write at list 2 characters to load more results",
+      isDisabled: true,
+    });
   }
   return (
     <>
@@ -119,6 +151,7 @@ export const Select = ({options, defaultValue, placeholder = 'Select...', isMult
         isSearchable={isSearchable}
         styles={customStyle}
         isMulti={isMulti}
+        captureMenuScroll={true}
         onMenuScrollToBottom={onMenuScrollToBottom}
         // menuPlacement='auto'
         onInputChange={handleInputChange}
@@ -126,5 +159,5 @@ export const Select = ({options, defaultValue, placeholder = 'Select...', isMult
         {...customComponents}
       />
     </>
-  )
-}
+  );
+};
