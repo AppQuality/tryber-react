ca290afd3c0a4253aa38cd12d01f8d0c3e680069
Davide Bizzi <davide.bizzi@app-quality.com>
2021-05-25 16:12:13 +0200

    feat: Add i18next translation parser

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import en from "./lang/en";
-import it from "./lang/it";
+import en from "./locales/en/translation.json";
+import it from "./locales/it/translation.json";
 // the translations
 // (tip move them in a JSON file and import them)
 const resources = {
