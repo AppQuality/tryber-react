29ea871e823e5a9c2443bd1c498c95907b2bf6bb
iacopolea <iacopo.leardini@app-quality.com>
2021-05-28 12:06:15 +0200

    feat: pagination style wrapper

--- src/stories/pagination/DesktopPagination.tsx
+++ src/stories/pagination/DesktopPagination.tsx
@@ -3,6 +3,7 @@ import { Button, ButtonGroup } from "../button/Button";
 import { generateShrinkedPages } from "./utils";
 import { MouseEventHandler } from "react";
 import { ChevronRight, ChevronLeft } from "react-bootstrap-icons";
+import styled from "styled-components";
 
 export const DesktopPagination = ({
   onPageChange,
@@ -21,36 +22,43 @@ export const DesktopPagination = ({
     pages = generateShrinkedPages(current, maxPages, size, onPageChange);
   }
 
+  const DesktopPaginationWrapper = styled.div`
+    margin-bottom: ${(props) => props.theme.grid.spacing.default};
+    text-align: center;
+  `;
+
   return (
-    <ButtonGroup>
-      <Button
-        flat={true}
-        squared={true}
-        onClick={() => onPageChange(current - 1)}
-        key="prev"
-        disabled={current === 1}
-        data-test-id="prev"
-      >
-        <ChevronLeft />
-      </Button>
-      {pages.map((i, idx) => {
-        let onClick: MouseEventHandler | undefined = () => i.action(i.page);
-        return (
-          <Button squared={true} flat={i.flat} onClick={onClick} key={idx}>
-            {i.page}
-          </Button>
-        );
-      })}
-      <Button
-        flat={true}
-        squared={true}
-        onClick={() => onPageChange(current + 1)}
-        key="next"
-        disabled={current === maxPages}
-        data-test-id="next"
-      >
-        <ChevronRight />
-      </Button>
-    </ButtonGroup>
+    <DesktopPaginationWrapper>
+      <ButtonGroup>
+        <Button
+          flat={true}
+          squared={true}
+          onClick={() => onPageChange(current - 1)}
+          key="prev"
+          disabled={current === 1}
+          data-test-id="prev"
+        >
+          <ChevronLeft />
+        </Button>
+        {pages.map((i, idx) => {
+          let onClick: MouseEventHandler | undefined = () => i.action(i.page);
+          return (
+            <Button squared={true} flat={i.flat} onClick={onClick} key={idx}>
+              {i.page}
+            </Button>
+          );
+        })}
+        <Button
+          flat={true}
+          squared={true}
+          onClick={() => onPageChange(current + 1)}
+          key="next"
+          disabled={current === maxPages}
+          data-test-id="next"
+        >
+          <ChevronRight />
+        </Button>
+      </ButtonGroup>
+    </DesktopPaginationWrapper>
   );
 };
