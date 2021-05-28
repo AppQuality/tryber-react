b894bd3a3e3f0d45734ad84eddaee930fb281c4c
Luca Cannarozzo <cannarozzoluca@live.it>
2021-05-28 15:42:43 +0200

    feat: add languages to sidebar

--- src/features/TesterSidebar.tsx
+++ src/features/TesterSidebar.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Sidebar } from "../stories/sidebar/Sidebar";
+import i18next from "i18next";
+import { BasicSidebarArgs } from "../stories/sidebar/Sidebar.stories.args";
+import { useTranslation } from "react-i18next";
 
 import {
   Laptop,
@@ -13,13 +16,25 @@ import {
   AwardFill,
   ChatDotsFill,
 } from "react-bootstrap-icons";
+
 export interface TesterSidebarProps {
   children?: React.ReactNode;
 }
 
-const BasicSidebarArgs = {
-  open: false,
-  onLogout: () => {
+export const DefaultLanguage = {
+  lang: "en",
+  onClick: (lang: string) => {
+    alert(`Switching to ${lang}`);
+    //Controllare se esiste.
+    i18next.changeLanguage(lang);
+  },
+};
+
+const TesterSidebar = ({ children }: TesterSidebarProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { t } = useTranslation();
+
+  BasicSidebarArgs.onLogout = () => {
     fetch("/wp-admin/admin-ajax.php?action=appq_wp_logout", {
       method: "GET",
     })
@@ -29,91 +44,82 @@ const BasicSidebarArgs = {
       .catch((e) => {
         alert(e.message);
       });
-  },
-  languages: {
-    current: { lang: "it" },
-    others: [
-      {
-        lang: "en",
-        onClick: (lang: string) => {
-          alert(`Switching to ${lang}`);
-        },
-      },
-      {
-        lang: "es",
-        onClick: (lang: string) => {
-          alert(`Switching to ${lang}`);
-        },
-      },
-    ],
-  },
-  items: [
+  };
+
+  BasicSidebarArgs.languages = {
+    current: { lang: i18next.language },
+    // eslint-disable-next-line eqeqeq
+    others: Array.from(
+      i18next.languages.filter((l) => l != i18next.language),
+      (lang) => {
+        return { ...DefaultLanguage, lang: lang };
+      }
+    ),
+  };
+
+  BasicSidebarArgs.items = [
     {
       url: "#",
       icon: <HouseFill />,
       active: true,
-      text: "Dashboard",
+      text: t("Dashboard"),
     },
     {
       url: "#",
       icon: <PersonFill />,
       active: false,
-      text: "Profile",
+      text: t("Profile"),
       last: true,
     },
     {
       url: "#",
       icon: <Laptop />,
       active: false,
-      text: "Devices",
+      text: t("Devices"),
     },
     {
       url: "#",
       icon: <Wallet2 />,
       active: false,
-      text: "Payments",
+      text: t("Payments"),
     },
     {
       url: "#",
       icon: <StarFill />,
       active: false,
-      text: "Experience Points",
+      text: t("Experience Points"),
     },
     {
       url: "#",
       icon: <GraphUp />,
       active: false,
-      text: "Leaderboard",
+      text: t("Leaderboard"),
     },
     {
       url: "#",
       icon: <BugFill />,
       active: false,
-      text: "Uploaded Bugs",
+      text: t("Uploaded Bugs"),
     },
     {
       url: "#",
       icon: <Eyeglasses />,
       active: false,
-      text: "Courses",
+      text: t("Courses"),
     },
     {
       url: "#",
       icon: <AwardFill />,
       active: false,
-      text: "Testing University",
+      text: t("Testing University"),
     },
     {
       url: "#",
       icon: <ChatDotsFill />,
       active: false,
-      text: "Feedback",
+      text: t("Feedback"),
     },
-  ],
-};
-
-const TesterSidebar = ({ children }: TesterSidebarProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+  ];
 
   return (
     <Sidebar
