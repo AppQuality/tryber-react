a988b2fbe5c9daa3b7673d979d47dafbe2cd9a76
Davide Bizzi <davide.bizzi@app-quality.com>
2021-06-11 14:02:13 +0200

    feat: Add countries to countryselect

--- src/features/CountrySelect.tsx
+++ src/features/CountrySelect.tsx
@@ -1,5 +1,6 @@
 import { Select, SelectType } from "@appquality/appquality-design-system";
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import countryList from "react-select-country-list";
 
 const CountrySelect = ({
   name,
@@ -11,17 +12,22 @@ const CountrySelect = ({
   onChange: (v: SelectType.Option) => void;
 }) => {
   const [value, setValue] = useState(initialValue);
-  const options = [
-    { label: "ciao", value: "ciao" },
-    { label: "miao", value: "miao" },
-  ];
-
+  const options = useMemo(
+    () =>
+      countryList()
+        .getData()
+        .map((c) => ({ label: c.label, value: c.label })),
+    []
+  );
   return (
     <Select
       name={name}
       label="Country"
       value={value}
       onChange={(v) => {
+        if (v == null) {
+          v = { label: "", value: "" };
+        }
         onChange(v);
         setValue(v);
       }}
