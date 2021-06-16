20f4775bf3ec3c2fc060532dff491975ab9e2863
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-06-16 12:15:17 +0200

    feat: login to WP-CROWD with nonce

---
+++ src/utils/wpapi.ts
@@ -0,0 +1,52 @@
+import queryString from "query-string";
+const WPAPI = {
+  login: ({
+    username,
+    password,
+    security,
+  }: {
+    username: string;
+    password: string;
+    security: string;
+  }) => {
+    return fetch(
+      `${process.env.REACT_APP_CROWD_WP_URL}/wp-admin/admin-ajax.php`,
+      {
+        method: "POST",
+        body: queryString.stringify({
+          username,
+          password,
+          security,
+          action: "ajaxlogin",
+        }),
+      }
+    )
+      .then((data) => data.json())
+      .then((res) => {
+        if (res.loggedin) {
+          return true;
+        }
+        throw new Error("Login failed.");
+      });
+  },
+  getNonce: () => {
+    return fetch(
+      `${process.env.REACT_APP_CROWD_WP_URL}/wp-admin/admin-ajax.php`,
+      {
+        method: "POST",
+        body: queryString.stringify({
+          action: "appq_get_nonce",
+        }),
+      }
+    )
+      .then((data) => data.json())
+      .then((res) => {
+        if (res.success) {
+          return res.data;
+        }
+        throw new Error("Nonce not found.");
+      });
+  },
+};
+
+export default WPAPI;
