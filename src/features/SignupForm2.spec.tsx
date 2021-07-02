ae2342f91da6bd0cd212a8d35e2275a0dde118f4
iacopolea <iacopo.leardini@app-quality.com>
2021-07-02 16:17:32 +0200

    test(signupform): add api test for signup form

--- src/features/SignupForm2.spec.tsx
+++ src/features/SignupForm2.spec.tsx
@@ -22,13 +22,21 @@ const mockedApi = mocked(API, true);
 const signupData = {
   name: "Pippo",
   surname: "Franco",
-  country: "italy",
-  birthDate: "18/04/1990",
+  country: "Italy",
+  birthDate: "1990-04-18",
   email: "pippo.franco@example.com",
   password: "pippoFranco0",
 };
 
-const weakPasswords = ["pippo", "pippofranco", "pippofranco0", "pippoFranco"];
+const weakPasswords = [
+  { pass: "pippo", message: "Must be at least 6 character long " },
+  { pass: "pippofranco", message: "Must contain at least a number " },
+  { pass: "pippoFranco", message: "Must contain at least a number " },
+  {
+    pass: "pippofranco0",
+    message: "Must contain at least an uppercase letter ",
+  },
+];
 const getFormFields = () => {
   const nameField = document.querySelector('input[name="name"]');
   const surnameField = document.querySelector('input[name="surname"]');
@@ -112,24 +120,115 @@ describe("Signup form: ", () => {
     ) {
       throw new Error("some field wasn't found");
     }
+    expect(submitButton).toHaveAttribute("disabled");
+
+    userEvent.type(nameField, signupData.name);
+    await waitFor(() => {
+      expect(submitButton).toHaveAttribute("disabled");
+    });
+
+    userEvent.type(surnameField, signupData.surname);
+    await waitFor(() => {
+      expect(submitButton).toHaveAttribute("disabled");
+    });
+
+    userEvent.type(mailField, signupData.email);
+    await waitFor(() => {
+      expect(submitButton).toHaveAttribute("disabled");
+    });
+
+    const _countrySelect = countrySelect as HTMLElement;
+    await selectEvent.select(_countrySelect, "Italy");
+    await waitFor(() => {
+      expect(submitButton).toHaveAttribute("disabled");
+    });
+
+    userEvent.click(datePicker);
+    await waitFor(async () => {
+      const overlay = document.querySelector(".mbsc-popup-overlay");
+      if (!overlay) throw new Error("datepicker not open");
+    });
+
+    const options = Array.prototype.slice.call(
+      document.querySelectorAll(".mbsc-scroller-wheel-item")
+    );
+    const buttons = Array.prototype.slice.call(
+      document.querySelectorAll(".mbsc-popup-button")
+    );
+    const Y = options.filter((option) => option.innerHTML === "1990");
+    const M = options.filter((option) => option.innerHTML === "April");
+    const D = options.filter((option) => option.innerHTML === "18");
+    const Set = buttons.filter((button) => button.innerHTML === "Set");
+
+    userEvent.click(Y[0]);
+    await waitFor(() => {});
+
+    userEvent.click(M[0]);
+    await waitFor(() => {});
+
+    userEvent.click(D[0]);
+    await waitFor(() => {});
+
+    userEvent.click(Set[0]);
+    await waitFor(() => {
+      expect(submitButton).toHaveAttribute("disabled");
+    });
+
+    userEvent.type(passwordField, signupData.password);
+    await waitFor(() => {
+      expect(form).toHaveFormValues({ password: signupData.password });
+    });
+    expect(submitButton).toHaveAttribute("disabled");
+
+    userEvent.click(tosCheckbox);
+    await waitFor(() => {
+      expect(tosCheckbox).toBeChecked();
+    });
+    expect(form).toHaveFormValues({ subscribe: true });
+    expect(submitButton).not.toHaveAttribute("disabled");
+  });
+
+  test("it should call the signup api on submit", async () => {
+    const [
+      nameField,
+      surnameField,
+      mailField,
+      countrySelect,
+      datePicker,
+      passwordField,
+      tosCheckbox,
+      submitButton,
+    ] = getFormFields();
+    const form = getForm();
+    // type guard for typescript, because queryselector can return null
+    if (
+      !nameField ||
+      !surnameField ||
+      !mailField ||
+      !countrySelect ||
+      !datePicker ||
+      !passwordField ||
+      !tosCheckbox ||
+      !submitButton
+    ) {
+      throw new Error("some field wasn't found");
+    }
+    mockedApi.signup.mockResolvedValueOnce(signupData);
 
     userEvent.type(nameField, signupData.name);
     await waitFor(() => {
       expect(form).toHaveFormValues({ name: signupData.name });
     });
-    expect(submitButton).toHaveAttribute("disabled");
 
     userEvent.type(surnameField, signupData.surname);
     await waitFor(() => {
       expect(form).toHaveFormValues({ surname: signupData.surname });
     });
-    expect(submitButton).toHaveAttribute("disabled");
 
     userEvent.type(mailField, signupData.email);
     await waitFor(() => {
       expect(form).toHaveFormValues({ email: signupData.email });
     });
-    expect(submitButton).toHaveAttribute("disabled");
 
     const _countrySelect = countrySelect as HTMLElement;
     await selectEvent.select(_countrySelect, "Italy");
@@ -156,19 +255,13 @@ describe("Signup form: ", () => {
     const Set = buttons.filter((button) => button.innerHTML === "Set");
 
     userEvent.click(Y[0]);
-    await waitFor(() => {
-      expect(Y[0]).toBeInTheDocument();
-    });
+    await waitFor(() => {});
 
     userEvent.click(M[0]);
-    await waitFor(() => {
-      expect(M[0]).toBeInTheDocument();
-    });
+    await waitFor(() => {});
 
     userEvent.click(D[0]);
-    await waitFor(() => {
-      expect(D[0]).toBeInTheDocument();
-    });
+    await waitFor(() => {});
 
     userEvent.click(Set[0]);
     await waitFor(() => {
@@ -179,13 +272,41 @@ describe("Signup form: ", () => {
     await waitFor(() => {
       expect(form).toHaveFormValues({ password: signupData.password });
     });
-    expect(submitButton).toHaveAttribute("disabled");
 
     userEvent.click(tosCheckbox);
     await waitFor(() => {
       expect(tosCheckbox).toBeChecked();
     });
     expect(form).toHaveFormValues({ subscribe: true });
-    expect(submitButton).not.toHaveAttribute("disabled");
+    userEvent.click(submitButton);
+    await waitFor(() => {
+      expect(API.signup).toHaveBeenCalledWith(signupData);
+      expect(API.signup).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("Weak Passwords:", () => {
+    test.each(weakPasswords)(
+      "writing %p as password, returns an invalid feedback",
+      async (weakPassword) => {
+        userEvent.type(screen.getByLabelText("Password"), weakPassword.pass);
+        screen.getByLabelText("Password").blur();
+        await waitFor(() => {
+          expect(screen.getByLabelText("Password")).toHaveValue(
+            weakPassword.pass
+          );
+        });
+        const label = document.querySelector("[for='signupForm-password']");
+        if (!label) throw new Error("there's no label");
+        const inputContainer = label.nextElementSibling;
+        if (!inputContainer) throw new Error("there's no input container");
+        const errorMessage = inputContainer.nextElementSibling;
+        if (!errorMessage) throw new Error("there's no error message");
+        expect(errorMessage).toHaveStyle(
+          `color: ${aqBootstrapTheme.palette.danger}`
+        );
+        expect(errorMessage.innerHTML).toBe(weakPassword.message);
+      }
+    );
   });
 });
