9b46ce235fa91f32c36c2a20dcdfb72138e4f2af
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-08-05 14:19:44 +0200

    feat: available-campaigns pt.2

--- src/features/dashboard/AvailableCampaignsTable.tsx
+++ src/features/dashboard/AvailableCampaignsTable.tsx
@@ -1,44 +1,51 @@
-import {Table, Pagination} from "@appquality/appquality-design-system";
+import { Table, Pagination } from "@appquality/appquality-design-system";
 import useAvailableCampaigns from "../../store/dashboard/useAvailableCampaigns";
 
 const AvailableCampaignsTable = () => {
-    const {campaigns, page, total} = useAvailableCampaigns();
+  const { campaigns, page, total } = useAvailableCampaigns();
 
-    return (
-        <>
-            <Table
-                dataSource={campaigns}
-                columns={[
-                    {
-                        title: 'Campaign',
-                        dataIndex: 'campaignName',
-                        key: 'campaignName'
-                    },
-                    {
-                        title: 'Type',
-                        dataIndex: 'type',
-                        key: 'type'
-                    },
-                    {
-                        title: 'Start Date',
-                        dataIndex: 'startDate',
-                        key: 'startDate',
-                    },
-                    {
-                        title: 'End Date',
-                        dataIndex: 'endDate',
-                        key: 'endDate',
-                    },
-                    {
-                        title: 'Actions',
-                        dataIndex: 'actions',
-                        key: 'actions'
-                    }
-                ]}
-            />
-            {(total >10 )? <Pagination onPageChange={page.set} current={page.current} maxPages={Math.ceil(total/10)}/> : null}
-        </>
-    );
+  return (
+    <>
+      <Table
+        dataSource={campaigns}
+        columns={[
+          {
+            title: "Campaign",
+            dataIndex: "campaignName",
+            key: "campaignName",
+          },
+          {
+            title: "Type",
+            dataIndex: "type",
+            key: "type",
+          },
+          {
+            title: "Start Date",
+            dataIndex: "startDate",
+            key: "startDate",
+          },
+          {
+            title: "End Date",
+            dataIndex: "endDate",
+            key: "endDate",
+          },
+          {
+            title: "Actions",
+            dataIndex: "actions",
+            key: "actions",
+            align: "center",
+          },
+        ]}
+      />
+      {total > 10 ? (
+        <Pagination
+          onPageChange={page.set}
+          current={page.current}
+          maxPages={Math.ceil(total / 10)}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default AvailableCampaignsTable;
