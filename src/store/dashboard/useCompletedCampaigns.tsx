147a88002bd4e5a9398303fa2cec85a1ba240db8
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-08-09 17:35:50 +0200

    feat: completed campaigns table

---
+++ src/store/dashboard/useCompletedCampaigns.tsx
@@ -0,0 +1,97 @@
+import { TableType } from "@appquality/appquality-design-system";
+import React, { useState, useEffect } from "react";
+import { operations } from "../../utils/schema";
+import API from "../../utils/api";
+import { useTranslation } from "react-i18next";
+
+export default () => {
+  const { i18n } = useTranslation();
+
+  const limit = 10;
+  const [campaigns, setCampaigns] = useState<TableType.Row[]>([]);
+  const [page, setPage] = useState(1);
+  const [totalEntries, setTotalEntries] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState<"ASC" | "DESC">("ASC");
+  const [orderBy, setOrderBy] = useState<"endDate" | "closeDate">("endDate");
+
+  const fetchCampaignsFromApi = (page: number) => {
+    return API.myCampaigns({
+      query: {
+        filterBy: {
+          accepted: "1",
+          completed: "1",
+        },
+        order: order,
+        orderBy: orderBy == "endDate" ? "end_date" : "close_date",
+        limit,
+        start: (page - 1) * limit,
+      },
+    }).then(
+      (
+        data: operations["get-users-me-campaigns"]["responses"]["200"]["content"]["application/json"]
+      ) => {
+        if (!data || !data.results) {
+          return { results: [], total: 0 };
+        }
+        const campaigns = data.results.map((cp) => {
+          const dateFormatter = (unformatted: string) => {
+            let d = new Date(unformatted);
+            return d.toLocaleString(i18n.language, {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            });
+          };
+          return {
+            key: cp.id ? cp.id : 123,
+            campaigns: cp.name,
+            endDate: dateFormatter(cp.dates.end),
+            closeDate: dateFormatter(cp.dates.close),
+          };
+        });
+        return {
+          results: campaigns,
+          total: data.total ? data.total : campaigns.length,
+        };
+      }
+    );
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetchCampaignsFromApi(page)
+      .then((data) => {
+        setCampaigns(data.results);
+        setTotalEntries(data.total);
+      })
+      .catch((e) => {
+        if (e.statusCode === 404) {
+          setCampaigns([]);
+          setTotalEntries(0);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [page, orderBy, order]);
+
+  return {
+    campaigns,
+    page: {
+      current: page,
+      set: setPage,
+    },
+    totalEntries,
+    limit,
+    loading,
+    order: {
+      current: order,
+      set: setOrder,
+    },
+    orderBy: {
+      current: orderBy,
+      set: setOrderBy,
+    },
+  };
+};
