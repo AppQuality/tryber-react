f0bfa058ab4b123d5cd9d1f62c3f12523d0eb76c
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-08-09 16:16:12 +0200

    feat: styled performance card

--- src/features/dashboard/PerformanceData.tsx
+++ src/features/dashboard/PerformanceData.tsx
@@ -1,45 +1,26 @@
-import { icons } from "@appquality/appquality-design-system";
+import {
+  icons,
+  BSGrid,
+  BSCol,
+  Text,
+  Spinner,
+  SpinnerWrapper,
+} from "@appquality/appquality-design-system";
 import usePerformance from "../../store/dashboard/usePerformance";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
+import i18n from "i18next";
 
-const StyledPerformance = styled.div`
-  .stats {
-    border-bottom: 1px solid #d1e0e8;
+const GoToBlock = styled.div`
+  border-bottom: 1px solid ${(props) => props.theme.colors.disabled};
+`;
+const StyledIcon = styled.div`
+  .dark-disabled-font {
+    color: ${(props) => props.theme.colors.disabledDark};
   }
-  .stats-row {
-    display: grid;
-    grid-template-columns: 21px 1fr 75px;
-  }
-  .stats-go-to {
-    display: grid;
-    grid-template-columns: 190px 21px;
-  }
-  .warning {
-    color: #ff9900;
-  }
-  .danger {
-    color: #d71116;
-  }
-  .success {
-    color: #3ead52;
-  }
-  .info {
-    color: #266a9a;
-  }
-  .primary {
-    color: #17405c;
-  }
-  .secondary {
-    color: #448098;
-  }
-  .icon {
-    width: 21px;
-    height: 21px;
-  }
-  .disabled-font {
-    color: #8ea2ae;
-  }
+`;
+const StyledAnchor = styled.a`
+  text-decoration: none;
 `;
 
 const PerformanceData = () => {
@@ -63,50 +44,157 @@ const PerformanceData = () => {
     loading,
   } = usePerformance();
   if (loading) {
-    return <div>LOADING CAZZO! ASPETTA!</div>;
+    return (
+      <SpinnerWrapper>
+        <Spinner />
+      </SpinnerWrapper>
+    );
   }
   return (
-    <StyledPerformance>
-      <div className={"stats"}>
-        <div className={"stats-row"}>
-          <StarFill className={"icon warning"} /> {t("Experience points")}{" "}
-          <strong>{expPoints}</strong>
-        </div>
-        <div className={"stats-row"}>
-          <AwardFill className={"icon info"} /> {t("Your Rank")}{" "}
-          <strong>{rank}</strong>
-        </div>
-        <a>
-          {t("View ranking page")} <ArrowRight className={"icon"} />
-        </a>
+    <>
+      <div className={"aq-mb-3"}>
+        <BSGrid>
+          <BSCol size={"col-2"}>
+            <StarFill size={"21"} className={"aq-text-warning"} />
+          </BSCol>
+          <BSCol size={"col-7"}>
+            <Text>{t("Experience Points")}</Text>
+          </BSCol>
+          <BSCol size={"col-3"}>
+            <Text className={"aq-text-right"}>
+              <strong>{expPoints}</strong>
+            </Text>
+          </BSCol>
+        </BSGrid>
       </div>
-      <div className={"stats"}>
-        <div className={"stats-row"}>
-          <BookmarkCheckFill className={"icon secondary"} />{" "}
-          {t("Completed campaigns")} <strong>{cpCompleted}</strong>
-        </div>
-        <div className={"stats-row"}>
-          <BugFill className={"icon danger"} /> {t("Approved bugs")}{" "}
-          <strong>{bugsApproved}</strong>
-        </div>
-        <a>
-          {t("View bugs page")} <ArrowRight className={"icon"} />
-        </a>
+      <div className={"aq-mb-3"}>
+        <BSGrid>
+          <BSCol size={"col-2"}>
+            <AwardFill size={"21"} className={"aq-text-info"} />
+          </BSCol>
+          <BSCol size={"col-7"}>
+            <Text>{t("Your Rank")}</Text>
+          </BSCol>
+          <BSCol size={"col-3"}>
+            <Text className={"aq-text-right"}>
+              <strong>{rank}</strong>
+            </Text>
+          </BSCol>
+        </BSGrid>
       </div>
-      <div className={"stats-no-border"}>
-        <div className={"stats-row"}>
-          <CashCoin className={"icon success"} /> {t("All-time booty")}{" "}
-          <strong>€ {allBooty}</strong>
-        </div>
-        <div className={"stats-row"}>
-          <PiggyBankFill className={"icon disabled-font"} />{" "}
-          {t("Reserved booty")} <strong>€ {pendingBooty}</strong>
-        </div>
-        <a className={"stats-go-to"}>
-          {t("View payments page")} <ArrowRight className={"icon"} />
-        </a>
+      <GoToBlock className={"aq-mb-3 aq-pb-3"}>
+        <StyledAnchor
+          href={`${window.location.host}/${
+            i18n.language !== "it" ? "it/leaderboard-2/" : "leaderboard"
+          }`}
+        >
+          <BSGrid>
+            <BSCol size={"col-9"}>
+              <span className={"aq-text-info"}>{t("View ranking page")}</span>
+            </BSCol>
+            <BSCol size={"col-3"}>
+              <ArrowRight className={"aq-float-right"} size={"21"} />
+            </BSCol>
+          </BSGrid>
+        </StyledAnchor>
+      </GoToBlock>
+
+      <div className={"aq-mb-3"}>
+        <BSGrid>
+          <BSCol size={"col-2"}>
+            <BookmarkCheckFill size={"21"} className={"aq-text-secondary"} />
+          </BSCol>
+          <BSCol size={"col-7"}>
+            <Text>{t("Completed campaigns")}</Text>
+          </BSCol>
+          <BSCol size={"col-3"}>
+            <Text className={"aq-text-right"}>
+              <strong>{cpCompleted}</strong>
+            </Text>
+          </BSCol>
+        </BSGrid>
       </div>
-    </StyledPerformance>
+      <div className={"aq-mb-3"}>
+        <BSGrid>
+          <BSCol size={"col-2"}>
+            <BugFill size={"21"} className={"aq-text-danger"} />
+          </BSCol>
+          <BSCol size={"col-7"}>
+            <Text>{t("Approved bugs")}</Text>
+          </BSCol>
+          <BSCol size={"col-3"}>
+            <Text className={"aq-text-right"}>
+              <strong>{bugsApproved}</strong>
+            </Text>
+          </BSCol>
+        </BSGrid>
+      </div>
+      <GoToBlock className={"aq-mb-3 aq-pb-3"}>
+        <StyledAnchor
+          href={`${window.location.host}/${
+            i18n.language !== "it" ? "it/" : ""
+          }my-bugs`}
+        >
+          <BSGrid>
+            <BSCol size={"col-9"}>
+              <span className={"aq-text-info"}>{t("View bugs page")}</span>
+            </BSCol>
+            <BSCol size={"col-3"}>
+              <ArrowRight className={"aq-float-right"} size={"21"} />
+            </BSCol>
+          </BSGrid>
+        </StyledAnchor>
+      </GoToBlock>
+
+      <div className={"aq-mb-3"}>
+        <BSGrid>
+          <BSCol size={"col-2"}>
+            <CashCoin size={"21"} className={"aq-text-success"} />
+          </BSCol>
+          <BSCol size={"col-6"}>
+            <Text>{t("All-time booty")}</Text>
+          </BSCol>
+          <BSCol size={"col-4"}>
+            <Text className={"aq-text-right"}>
+              <strong>€ {allBooty}</strong>
+            </Text>
+          </BSCol>
+        </BSGrid>
+      </div>
+      <div className={"aq-mb-3"}>
+        <BSGrid>
+          <BSCol size={"col-2"}>
+            <StyledIcon>
+              <PiggyBankFill size={"21"} className={"dark-disabled-font"} />
+            </StyledIcon>
+          </BSCol>
+          <BSCol size={"col-6"}>
+            <Text>{t("Reserved booty")}</Text>
+          </BSCol>
+          <BSCol size={"col-4"}>
+            <Text className={"aq-text-right"}>
+              <strong>€ {pendingBooty}</strong>
+            </Text>
+          </BSCol>
+        </BSGrid>
+      </div>
+      <div className={"aq-mb-3"}>
+        <StyledAnchor
+          href={`${window.location.host}/${
+            i18n.language !== "it" ? "it/pagamenti/" : "payments/"
+          }`}
+        >
+          <BSGrid>
+            <BSCol size={"col-9"}>
+              <span className={"aq-text-info"}>{t("View payments page")}</span>
+            </BSCol>
+            <BSCol size={"col-3"}>
+              <ArrowRight className={"aq-float-right"} size={"21"} />
+            </BSCol>
+          </BSGrid>
+        </StyledAnchor>
+      </div>
+    </>
   );
 };
 
