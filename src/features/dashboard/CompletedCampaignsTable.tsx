ae64bee33531ba199cb5cecb658f1b1f696c49e3
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-08-09 17:35:50 +0200

    feat: completed campaigns table

--- src/features/dashboard/CompletedCampaignsTable.tsx
+++ src/features/dashboard/CompletedCampaignsTable.tsx
@@ -1,32 +1,56 @@
-import {Table} from "@appquality/appquality-design-system";
+import { Pagination, Table } from "@appquality/appquality-design-system";
+import { useTranslation } from "react-i18next";
+import useCompletedCampaigns from "../../store/dashboard/useCompletedCampaigns";
 
 const CompletedCampaignsTable = () => {
-    return (
-        <>
-            <Table
-                dataSource={[]}
-                columns={[
-                    {
-                        title: 'Campaign',
-                        dataIndex: 'campaigns',
-                        key: 'campaigns'
-                    },
-                    {
-                        title: 'End date',
-                        dataIndex: 'endate',
-                        key: 'endate',
-                    },
-                    {
-                        title: 'Close date',
-                        dataIndex: 'closedate',
-                        key: 'closedate',
-                    }
-                ]}
-            />
-            {/*//if rows are more than 10 show pagination*/}
-            {/*<Pagination/>*/}
-        </>
-    );
+  const { t } = useTranslation();
+  const { campaigns, page, totalEntries, limit, loading, order, orderBy } =
+    useCompletedCampaigns();
+  return (
+    <>
+      <Table
+        dataSource={campaigns}
+        isLoading={loading}
+        isStriped={true}
+        order={order.current}
+        orderBy={orderBy.current}
+        columns={[
+          {
+            title: t("Campaign"),
+            dataIndex: "campaigns",
+            key: "campaigns",
+          },
+          {
+            title: t("End Date"),
+            dataIndex: "endDate",
+            key: "endDate",
+            isSortable: true,
+            onSort: (sorting: "ASC" | "DESC") => {
+              order.set(sorting);
+              orderBy.set("endDate");
+            },
+          },
+          {
+            title: t("Close Date"),
+            dataIndex: "closeDate",
+            key: "closeDate",
+            isSortable: true,
+            onSort: (sorting: "ASC" | "DESC") => {
+              order.set(sorting);
+              orderBy.set("closeDate");
+            },
+          },
+        ]}
+      />
+      {totalEntries > limit ? (
+        <Pagination
+          onPageChange={page.set}
+          current={page.current}
+          maxPages={Math.ceil(totalEntries / limit)}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default CompletedCampaignsTable;
