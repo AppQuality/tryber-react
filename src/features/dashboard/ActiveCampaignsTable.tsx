f6d0b72cd420afcfe95685813be08d5f7fc9faae
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-08-09 17:14:11 +0200

    feat: active campaigns table

--- src/features/dashboard/ActiveCampaignsTable.tsx
+++ src/features/dashboard/ActiveCampaignsTable.tsx
@@ -1,41 +1,53 @@
-import {
-    Table
-} from "@appquality/appquality-design-system";
-import {campaigns} from "../../utils/campaigns";
+import { Pagination, Table } from "@appquality/appquality-design-system";
+import useActiveCampaigns from "../../store/dashboard/useActiveCampaigns";
+import { useTranslation } from "react-i18next";
 
 const ActiveCampaignsTable = () => {
+  const { t } = useTranslation();
+  const { campaigns, page, totalEntries, limit, loading, order, orderBy } =
+    useActiveCampaigns();
 
-    return (
-        <>
-            <Table
-                dataSource={[]}
-                columns={[
-                    {
-                        title: 'Campaign',
-                        dataIndex: 'campaigns',
-                        key: 'campaigns'
-                    },
-                    {
-                        title: 'End Date',
-                        dataIndex: 'endate',
-                        key: 'endate',
-                        isSortable: true,
-                        // onSort: (sorting: "ASC" | "DESC") => {
-                        //     order.set(sorting);
-                        //     orderBy.set("endate");
-                        // },
-                    },
-                    {
-                        title: 'Actions',
-                        dataIndex: '',
-                        key: 'actions'
-                    }
-                ]}
-            />
-            {/*//if rows are more than 10 show pagination*/}
-            {/*<Pagination/>*/}
-        </>
-    );
+  return (
+    <>
+      <Table
+        dataSource={campaigns}
+        isLoading={loading}
+        isStriped={true}
+        order={order.current}
+        orderBy={orderBy.current}
+        columns={[
+          {
+            title: t("Campaign"),
+            dataIndex: "campaigns",
+            key: "campaigns",
+          },
+          {
+            title: t("End Date"),
+            dataIndex: "endDate",
+            key: "endDate",
+            isSortable: true,
+            onSort: (sorting: "ASC" | "DESC") => {
+              order.set(sorting);
+              orderBy.set("endDate");
+            },
+          },
+          {
+            title: t("Actions"),
+            dataIndex: "actions",
+            key: "actions",
+            align: "center",
+          },
+        ]}
+      />
+      {totalEntries > limit ? (
+        <Pagination
+          onPageChange={page.set}
+          current={page.current}
+          maxPages={Math.ceil(totalEntries / limit)}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default ActiveCampaignsTable;
