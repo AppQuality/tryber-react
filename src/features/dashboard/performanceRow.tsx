ef50e4ac9c12a5f8762490b011f8bb22deb350cc
Davide Bizzi <davide.bizzi@app-quality.com>
2021-08-23 13:35:35 +0200

    feat(dashboard): Remove infinite from performance data arrow animation

--- src/features/dashboard/performanceRow.tsx
+++ src/features/dashboard/performanceRow.tsx
@@ -1,5 +1,5 @@
-import {Text} from "@appquality/appquality-design-system";
-import {BaseProps} from "@appquality/appquality-design-system/dist/shared/_types";
+import { Text } from "@appquality/appquality-design-system";
+import { BaseProps } from "@appquality/appquality-design-system/dist/shared/_types";
 import React from "react";
 import styled from "styled-components";
 
@@ -9,29 +9,31 @@ interface PerformanceRowProps extends BaseProps {
     text: React.ReactNode;
     val?: React.ReactNode;
     link?: string;
-  },
+  };
 }
 
-const StatisticComponent = ({item, className}: PerformanceRowProps) => {
+const StatisticComponent = ({ item, className }: PerformanceRowProps) => {
   return (
     <div className={`${className} aq-mb-3`}>
-      {item.icon && <div className='performance-icon aq-mr-3'>{item.icon}</div>}
-      <Text className='performance-text aq-text-secondary aq-mr-3'>{item.text}</Text>
+      {item.icon && <div className="performance-icon aq-mr-3">{item.icon}</div>}
+      <Text className="performance-text aq-text-secondary aq-mr-3">
+        {item.text}
+      </Text>
       <Text className="performance-val">
         <strong>{item.val}</strong>
       </Text>
     </div>
-  )
-}
+  );
+};
 
-const GoToBlockComponent = ({item, className}: PerformanceRowProps) => {
+const GoToBlockComponent = ({ item, className }: PerformanceRowProps) => {
   return (
     <a href={item.link} className={`${className} aq-text-info`}>
       <span>{item.text}</span>
-      <span className='go-to-icon'>{item.icon}</span>
+      <span className="go-to-icon">{item.icon}</span>
     </a>
-  )
-}
+  );
+};
 
 export const Statistic = styled(StatisticComponent)`
   display: flex;
@@ -51,29 +53,17 @@ export const GoToBlock = styled(GoToBlockComponent)`
   display: flex;
   justify-content: space-between;
   text-decoration: none;
-  
+  .go-to-icon {
+    transition: transform 0.1s;
+  }
   &:not(:last-of-type) {
-    border-bottom: 1px solid ${props => props.theme.colors.gray300};
-    margin-bottom: ${props => props.theme.grid.spacing.default};
-    padding-bottom: ${props => props.theme.grid.spacing.default};
+    border-bottom: 1px solid ${(props) => props.theme.colors.gray300};
+    margin-bottom: ${(props) => props.theme.grid.spacing.default};
+    padding-bottom: ${(props) => props.theme.grid.spacing.default};
   }
   &:hover {
     text-decoration: underline;
     .go-to-icon {
-      animation-name: bounce-right;
-      animation-duration: .8s;
-      animation-timing-function: ease-in;
-      animation-iteration-count: infinite;
-    }
-  }
-  @keyframes bounce-right {
-    0% {
-      transform: translate3d(0, 0, 0);
-    }
-    20% {
-      transform: translate3d(20%, 0, 0);
-    }
-    100% {
       transform: translate3d(20%, 0, 0);
     }
   }
