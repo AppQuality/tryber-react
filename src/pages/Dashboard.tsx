b2346d8b4fc2e9e9078c4c139dc1e5ab97882c4d
Davide Bizzi <davide.bizzi@app-quality.com>
2021-08-24 10:42:55 +0200

    rework(dashboard): Use gtm and logged components

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -24,6 +24,9 @@ import PopupContainer from "../features/dashboard/PopupContainer";
 import { useTranslation } from "react-i18next";
 import { useUser } from "../store/useUser";
 
+import GoogleTagManager from "../features/GoogleTagManager";
+import LoggedOnly from "../features/LoggedOnly";
+
 const tagManagerArgs = {
   dataLayer: {
     role: "unknown",
@@ -36,149 +39,101 @@ const tagManagerArgs = {
 
 export default function Dashboard() {
   //constants - START
-
-  const { user, error, isLoading } = useUser();
-  const [isAdmin, setIsAdmin] = useState(false);
   const [isPopupModalOpen, setIsPopupModalOpen] = useState(true);
   const [isPopupArchiveModalOpen, setIsPopupArchiveModalOpen] = useState(false);
 
   const { t } = useTranslation();
 
-  const helmet = () => {
-    return (
-      <Helmet>
-        <title>{t("Dashboard")} - AppQuality Crowd</title>
-        <meta property="og:title" content={t("Dashboard")} />
-        <meta name="description" content={t("Dashboard")} />
-      </Helmet>
-    );
-  };
-  //constants - END
-
-  useEffect(() => {
-    if (user) {
-      tagManagerArgs.dataLayer = {
-        role: user.role,
-        wp_user_id: user.wp_user_id,
-        tester_id: user.id,
-        is_admin_page: false,
-      };
-      setIsAdmin(["administrator", "tester_lead"].includes(user.role));
-    } else {
-      if (error) {
-        if (error.statusCode === 403) {
-          window.location.href = "/";
-        } else {
-          alert(error.message);
-        }
-      }
-    }
-  }, [user, error]);
-
-  if (isLoading) {
-    return (
-      <>
-        {helmet()}
-        <Container className="aq-py-3">
-          <SpinnerWrapper>
-            <Spinner />
-            <Title size="xs" as="h5">
-              {t("Loading")}
-            </Title>
-          </SpinnerWrapper>
-        </Container>
-      </>
-    );
-  }
   return (
-    <>
-      {helmet()}
-      <PopupContainer
-        open={isPopupModalOpen}
-        onClose={() => setIsPopupModalOpen(false)}
-      />
-      <PopupContainer
-        onClose={() => setIsPopupArchiveModalOpen(false)}
-        open={isPopupArchiveModalOpen}
-        showExpired={true}
-      />
-      <TesterSidebar route={"my-dashboard"}>
-        <Container className="aq-pb-3">
-          <PageTitle
-            as="h2"
-            size="regular"
-            subtitle={t(
-              "This is your personal dashboard. From here you can check out your stats, keep an eye on the progress of your work and find new campaigns to apply for. Have fun!"
-            )}
-          >
-            {t("Dashboard")}
-          </PageTitle>
-          <BSGrid>
-            <BSCol size="col-lg-9 ">
-              <Card className="aq-mb-3" bodyClass="">
-                <Tabs active="active">
-                  <Tab id="active" title={t("To be completed")}>
-                    <div className="aq-m-3">
-                      <Text>
-                        {t(
-                          `A list of all the campaigns you successfully applied for. Read the manual and report as many bugs as you can to earn experience points and earn money. Don't forget to apply for new campaigns!`
-                        )}
-                      </Text>
-                      <ActiveCampaignsTable />
-                    </div>
-                  </Tab>
-                  <Tab id="completed" title={t("Completed")}>
-                    <div className="aq-m-3">
-                      <Text>
-                        {t(
-                          `A list of all the campaigns you participated in that are now completed. We will evaluate your performance in the 14 days following the end of the campaign and award you experience points and money accordingly.`
-                        )}
-                      </Text>
-                      <CompletedCampaignsTable />
-                    </div>
-                  </Tab>
-                  <Tab id="closed" title={t("Closed")}>
-                    <div className="aq-m-3">
-                      <Text>
-                        {t(
-                          `A list of the campaigns you particiated in that were successfully evaluated by us. This list is designed to help you keep track of your activities as a tester.`
-                        )}
-                      </Text>
-                      <ClosedCampaignsTable />
-                    </div>
-                  </Tab>
-                </Tabs>
-              </Card>
-              <Card className="aq-mb-3" title={t("Avalaible Campaigns")}>
-                <AvailableCampaignsTable />
-              </Card>
-            </BSCol>
-            <BSCol size="col-lg-3">
-              <div className="stick-to-header-lg ">
-                <Card
-                  className="aq-mb-3"
-                  title={t("Your Performance")}
-                  shadow={true}
-                >
-                  <PerformanceData />
+    <GoogleTagManager title={t("Dashboard")}>
+      <LoggedOnly>
+        <PopupContainer
+          open={isPopupModalOpen}
+          onClose={() => setIsPopupModalOpen(false)}
+        />
+        <PopupContainer
+          onClose={() => setIsPopupArchiveModalOpen(false)}
+          open={isPopupArchiveModalOpen}
+          showExpired={true}
+        />
+        <TesterSidebar route={"my-dashboard"}>
+          <Container className="aq-pb-3">
+            <PageTitle
+              as="h2"
+              size="regular"
+              subtitle={t(
+                "This is your personal dashboard. From here you can check out your stats, keep an eye on the progress of your work and find new campaigns to apply for. Have fun!"
+              )}
+            >
+              {t("Dashboard")}
+            </PageTitle>
+            <BSGrid>
+              <BSCol size="col-lg-9 ">
+                <Card className="aq-mb-3" bodyClass="">
+                  <Tabs active="active">
+                    <Tab id="active" title={t("To be completed")}>
+                      <div className="aq-m-3">
+                        <Text>
+                          {t(
+                            `A list of all the campaigns you successfully applied for. Read the manual and report as many bugs as you can to earn experience points and earn money. Don't forget to apply for new campaigns!`
+                          )}
+                        </Text>
+                        <ActiveCampaignsTable />
+                      </div>
+                    </Tab>
+                    <Tab id="completed" title={t("Completed")}>
+                      <div className="aq-m-3">
+                        <Text>
+                          {t(
+                            `A list of all the campaigns you participated in that are now completed. We will evaluate your performance in the 14 days following the end of the campaign and award you experience points and money accordingly.`
+                          )}
+                        </Text>
+                        <CompletedCampaignsTable />
+                      </div>
+                    </Tab>
+                    <Tab id="closed" title={t("Closed")}>
+                      <div className="aq-m-3">
+                        <Text>
+                          {t(
+                            `A list of the campaigns you particiated in that were successfully evaluated by us. This list is designed to help you keep track of your activities as a tester.`
+                          )}
+                        </Text>
+                        <ClosedCampaignsTable />
+                      </div>
+                    </Tab>
+                  </Tabs>
                 </Card>
-                <Card shadow={true}>
-                  <Button
-                    flat
-                    type="info"
-                    size="block"
-                    onClick={() =>
-                      setIsPopupArchiveModalOpen(!isPopupArchiveModalOpen)
-                    }
+                <Card className="aq-mb-3" title={t("Avalaible Campaigns")}>
+                  <AvailableCampaignsTable />
+                </Card>
+              </BSCol>
+              <BSCol size="col-lg-3">
+                <div className="stick-to-header-lg ">
+                  <Card
+                    className="aq-mb-3"
+                    title={t("Your Performance")}
+                    shadow={true}
                   >
-                    {t("Message archive")}
-                  </Button>
-                </Card>
-              </div>
-            </BSCol>
-          </BSGrid>
-        </Container>
-      </TesterSidebar>
-    </>
+                    <PerformanceData />
+                  </Card>
+                  <Card shadow={true}>
+                    <Button
+                      flat
+                      type="info"
+                      size="block"
+                      onClick={() =>
+                        setIsPopupArchiveModalOpen(!isPopupArchiveModalOpen)
+                      }
+                    >
+                      {t("Message archive")}
+                    </Button>
+                  </Card>
+                </div>
+              </BSCol>
+            </BSGrid>
+          </Container>
+        </TesterSidebar>
+      </LoggedOnly>
+    </GoogleTagManager>
   );
 }
