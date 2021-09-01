60d897afac35138e4ef8a63a093550879178f48d
Davide Bizzi <davide.bizzi@app-quality.com>
2021-09-01 13:17:56 +0200

    i18n: Change copy

--- src/features/dashboard/CampaignTableCard.tsx
+++ src/features/dashboard/CampaignTableCard.tsx
@@ -1,84 +1,84 @@
 import {
-    Table,
-    Pagination,
-    TableType,
+  Table,
+  Pagination,
+  TableType,
 } from "@appquality/appquality-design-system";
 import { useTranslation } from "react-i18next";
 
 interface CampaignsTableProps {
-    data: TableType.Row[];
-    page: number;
-    setPage: (page: number) => void;
-    totalEntries: number;
-    limit: number;
-    loading: boolean;
-    order: any;
-    orderBy: any;
+  data: TableType.Row[];
+  page: number;
+  setPage: (page: number) => void;
+  totalEntries: number;
+  limit: number;
+  loading: boolean;
+  order: any;
+  orderBy: any;
 }
 
 const CampaignsTable = ({
-                                   data,
-                                   page,
-                                   setPage,
-                                   totalEntries,
-                                   limit,
-                                   loading,
-                                   order,
-                                   orderBy,
-                               }: CampaignsTableProps) => {
-    const { t } = useTranslation();
-    const columns: TableType.Column[] = [
-        {
-            title: t("Campaign"),
-            dataIndex: "amount",
-            key: "amount",
-            width: "10ch",
-            isSortable: true,
-            onSort: (sorting: "ASC" | "DESC") => {
-                order.set(sorting);
-                orderBy.set("amount");
-            },
-        },
-        {
-            title: t("End Date"),
-            dataIndex: "date",
-            key: "date",
-            width: "8ch",
-            isSortable: true,
-            onSort: (sorting: "ASC" | "DESC") => {
-                order.set(sorting);
-                orderBy.set("date");
-            },
-        },
-        {
-            title: t("Actions"),
-            dataIndex: "activity",
-            key: "activity",
-            width: "10ch",
-        },
-    ];
-    return (
-        <>
-            <Table
-                dataSource={data}
-                columns={columns}
-                orderBy={orderBy.current}
-                order={order.current}
-                isLoading={loading}
-                isStriped
-            />
-            <Pagination
-                onPageChange={setPage}
-                current={page}
-                maxPages={Math.ceil(totalEntries / limit)}
-                mobileText={(current, total) =>
-                    t(`Page %current% / %total%`)
-                        .replace("%current%", current.toString())
-                        .replace("%total%", total ? total.toString() : "0")
-                }
-            />
-        </>
-    );
+  data,
+  page,
+  setPage,
+  totalEntries,
+  limit,
+  loading,
+  order,
+  orderBy,
+}: CampaignsTableProps) => {
+  const { t } = useTranslation();
+  const columns: TableType.Column[] = [
+    {
+      title: t("Campaign"),
+      dataIndex: "amount",
+      key: "amount",
+      width: "10ch",
+      isSortable: true,
+      onSort: (sorting: "ASC" | "DESC") => {
+        order.set(sorting);
+        orderBy.set("amount");
+      },
+    },
+    {
+      title: t("End Date"),
+      dataIndex: "date",
+      key: "date",
+      width: "8ch",
+      isSortable: true,
+      onSort: (sorting: "ASC" | "DESC") => {
+        order.set(sorting);
+        orderBy.set("date");
+      },
+    },
+    {
+      title: t("Action"),
+      dataIndex: "activity",
+      key: "activity",
+      width: "10ch",
+    },
+  ];
+  return (
+    <>
+      <Table
+        dataSource={data}
+        columns={columns}
+        orderBy={orderBy.current}
+        order={order.current}
+        isLoading={loading}
+        isStriped
+      />
+      <Pagination
+        onPageChange={setPage}
+        current={page}
+        maxPages={Math.ceil(totalEntries / limit)}
+        mobileText={(current, total) =>
+          t(`Page %current% / %total%`)
+            .replace("%current%", current.toString())
+            .replace("%total%", total ? total.toString() : "0")
+        }
+      />
+    </>
+  );
 };
 
 export default CampaignsTable;
