7f24c9aca669160f7339adf28dc8dd3e15b4129a
Davide Bizzi <davide.bizzi@app-quality.com>
2021-09-09 10:09:35 +0200

    feat: Add basic structure for devices

---
+++ src/pages/Devices.tsx
@@ -0,0 +1,58 @@
+import TesterSidebar from "../features/TesterSidebar";
+import {
+  Container,
+  BSGrid,
+  BSCol,
+  Card,
+  Text,
+  PageTitle,
+  Button,
+} from "@appquality/appquality-design-system";
+import GoogleTagManager from "../features/GoogleTagManager";
+import LoggedOnly from "../features/LoggedOnly";
+import { useTranslation } from "react-i18next";
+
+const Devices = () => {
+  const { t } = useTranslation();
+  return (
+    <GoogleTagManager title={t("Personal Equipment")}>
+      <LoggedOnly>
+        <TesterSidebar route={"personal-equipment"}>
+          <Container className="aq-pb-3">
+            <PageTitle size="regular" as="h2" className="aq-mb-3">
+              {t("Devices")}
+            </PageTitle>
+            <BSGrid>
+              <BSCol size="col-lg-9 aq-order-1 aq-order-0-lg ">
+                <Card className="aq-mb-3" title={t("Your devices")}>
+                  <Text>
+                    {t(
+                      "Here is the list of your all devices. Make sure to keep it update in order to boost your chances to be selected for further projects."
+                    )}
+                  </Text>
+                </Card>
+              </BSCol>
+              <BSCol size="col-lg-3">
+                <Card
+                  className="stick-to-header-lg aq-mb-3"
+                  title={t("Add device")}
+                  shadow={true}
+                >
+                  <Text className="aq-mb-3">
+                    {t(
+                      "Adding more devices allows you to apply to more campaigns to earn more money and experience points."
+                    )}
+                  </Text>
+                  <Button flat={true} type="success" size="block">
+                    {t("Add device")}
+                  </Button>
+                </Card>
+              </BSCol>
+            </BSGrid>
+          </Container>
+        </TesterSidebar>
+      </LoggedOnly>
+    </GoogleTagManager>
+  );
+};
+export default Devices;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,6 @@ import MyBugs from "./MyBugs";
 import ExperiencePoints from "./ExperiencePoints";
 import GettingStarted from "./GettingStarted";
 import Home from "./home/Home";
+import Devices from "./Devices";
 
-export { Dashboard, GettingStarted, MyBugs, ExperiencePoints, Home };
+export { Dashboard, GettingStarted, MyBugs, ExperiencePoints, Home, Devices };
