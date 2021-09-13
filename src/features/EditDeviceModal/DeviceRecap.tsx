72d620aeff842c14b8447b9440209d198321ade5
iacopolea <iacopo.lea@gmail.com>
2021-09-13 13:07:03 +0200

    wip(device): add recap

--- src/features/EditDeviceModal/DeviceRecap.tsx
+++ src/features/EditDeviceModal/DeviceRecap.tsx
@@ -1,7 +1,65 @@
-import { Select } from "@appquality/appquality-design-system";
-import userDeviceStore from "../../redux/userDevices";
+import { useFormikContext } from "formik";
+import { Laptop, Phone, Tablet, Tv } from "react-bootstrap-icons";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 
 export default () => {
-  const { current } = userDeviceStore();
-  return <>{JSON.stringify(current)}</>;
+  const { values } = useFormikContext<DeviceFormInterface>();
+  const { t } = useTranslation();
+  const getIcon = () => {
+    switch (values.device_type) {
+      case 0:
+        return <Phone />;
+      case 1:
+        return <Tablet />;
+      case 2:
+        return <Laptop />;
+      case 5:
+        return <Tv />;
+      default:
+        return "";
+    }
+  };
+  return (
+    <RecapBurrito>
+      <div>{getIcon()}</div>
+      <ul>
+        {values.device_type === 2 ? (
+          <li>
+            <span>{t("Computer type")}:</span> <strong>{values.device}</strong>
+          </li>
+        ) : (
+          <>
+            <li>
+              <span>{t("Manufacturer")}:</span>{" "}
+              <strong>{values.manufacturer}</strong>
+            </li>
+            <li>
+              <span>{t("Model")}:</span> <strong>{values.model}</strong>
+            </li>
+          </>
+        )}
+        <li>
+          <span>{t("Operating system")}:</span>{" "}
+          <strong>{values.operating_system_platform}</strong>
+        </li>
+        <li>
+          <span>{t("Operating system version")}:</span>{" "}
+          <strong>{values.operating_system_version}</strong>
+        </li>
+      </ul>
+    </RecapBurrito>
+  );
 };
+
+const RecapBurrito = styled.div`
+  svg {
+    width: 56px;
+    height: 56px;
+    color: ${(props) => props.theme.palette.secondary};
+  }
+  @media (min-width: ${(props) => props.theme.grid.breakpoints.lg}) {
+    display: flex;
+    align-items: flex-start;
+  }
+`;
--- src/features/EditDeviceModal/types.d.ts
+++ src/features/EditDeviceModal/types.d.ts
@@ -10,7 +10,7 @@ interface DeviceFormInterface {
         | "Gaming PC"
         | "Tablet PC / Hybrid"
       );
-  device_type: number; // 0 smartphone, 1 tablet, 2 pc, 5 smartTv
+  device_type: 0 | 1 | 2 | 3 | 4 | 5; // 0 smartphone, 1 tablet, 2 pc, 3 console, 4 smartwatch,  5 smartTv
   operating_system_platform: string;
   operating_system_version: string;
   operating_system_id: string;
