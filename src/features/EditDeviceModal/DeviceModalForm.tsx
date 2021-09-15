b43597c573a076fae6e9c0ff7e808ed2f6d9d864
Davide Bizzi <davide.bizzi@app-quality.com>
2021-09-15 12:39:32 +0200

    feat(device): Use custom message when not modified device

--- src/features/EditDeviceModal/DeviceModalForm.tsx
+++ src/features/EditDeviceModal/DeviceModalForm.tsx
@@ -88,7 +88,17 @@ export const DeviceModalForm = ({ children, step, closeModal }: FormProps) => {
               })
               .catch((e) => {
                 if (e.statusCode === 304) {
-                  add({ message: e.message, type: "info" });
+                  add({
+                    message: (
+                      <div>
+                        <strong>{t(`Your device was not modified`)}</strong>
+                        <div>
+                          {t(`You didn't change anything on your device`)}
+                        </div>
+                      </div>
+                    ),
+                    type: "info",
+                  });
                 } else {
                   add({
                     message: e.message || t("Generic error"),
