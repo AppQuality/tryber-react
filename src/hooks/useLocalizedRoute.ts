c00844ada35c57ccf7c36a84f0026352bd96e9f9
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-10-07 10:48:34 +0200

    feat: improve CrowdRoutes types

--- src/hooks/useLocalizedRoute.ts
+++ src/hooks/useLocalizedRoute.ts
@@ -1,16 +1,11 @@
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { CrowdRoutes } from "../types";
 
-type CrowdRoutes = "getting-started" | "my-dashboard" | "";
-
-export function useLocalizeRoute(route: string): string {
+export function useLocalizeRoute(route: CrowdRoutes): string {
   const { i18n } = useTranslation();
-  const [path, setPath] = useState(route);
-
-  useEffect(() => {
-    const localizedPath =
-      i18n.language === "en" ? `/${route}` : `/${i18n.language}/${route}/`;
-    setPath(localizedPath);
-  }, [i18n.language]);
-  return path;
+  let localizedRoute =
+    i18n.language === "en" ? `/${route}` : `/${i18n.language}/${route}`;
+  // in case of base route ("") we already have a forward slash
+  let re = /\/$/;
+  return re.test(localizedRoute) ? localizedRoute : `${localizedRoute}/`;
 }
