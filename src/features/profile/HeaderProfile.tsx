8b77eaf4f4c1fbe0283ee6411e660ef6097becdd
iacopolea <iacopo.lea@gmail.com>
2021-10-12 17:56:44 +0200

    feat(profile): add header profile style responsive

--- src/features/profile/HeaderProfile.tsx
+++ src/features/profile/HeaderProfile.tsx
@@ -3,44 +3,61 @@ import UserStore from "../../redux/user";
 import {
   BSCol,
   BSGrid,
+  CSSGrid,
   PageTitle,
   Title,
 } from "@appquality/appquality-design-system";
 import { StarFill, BookmarkCheckFill, Mailbox } from "react-bootstrap-icons";
 import { aqBootstrapTheme } from "@appquality/appquality-design-system";
+import styled from "styled-components";
 
 export const HeaderProfile = () => {
   const { t } = useTranslation();
-  const { user, isProfileLoading } = UserStore();
+  const { user } = UserStore();
   return (
-    <>
-      <BSGrid>
-        <BSCol size="col-3">
-          <img className="avatar" src={user.image}></img>
-        </BSCol>
-        <BSCol size="col-5">
-          <PageTitle size="small" subtitle={"T" + user.id}>
-            {`${user.name} ${user.surname}`}
-          </PageTitle>
-          <div>
-            <StarFill color={aqBootstrapTheme.palette.warning} />{" "}
-            <strong>{user.total_exp_pts}</strong> pt
-          </div>
-          <div>
-            <BookmarkCheckFill color={aqBootstrapTheme.palette.secondary} />{" "}
-            <strong>{user.attended_cp}</strong> {t("Campagne completate")}
-          </div>
-        </BSCol>
-        <BSCol size="col-4">
-          <div>
-            <Title size="s">{t("Stato email")}</Title>
-          </div>
-          <div>
-            <Mailbox color={aqBootstrapTheme.palette.info} />{" "}
-            {t("Conferma l'email")}
-          </div>
-        </BSCol>
-      </BSGrid>
-    </>
+    <StyledHeaderProfile className="aq-m-3">
+      <div>
+        <img className="profile-avatar" src={user.image} alt={user.name} />
+      </div>
+      <div>
+        <PageTitle size="small" subtitle={"T" + user.id}>
+          {`${user.name} ${user.surname}`}
+        </PageTitle>
+        <div>
+          <StarFill color={aqBootstrapTheme.palette.warning} />{" "}
+          <strong>{user.total_exp_pts}</strong> pt
+        </div>
+        <div>
+          <BookmarkCheckFill color={aqBootstrapTheme.palette.secondary} />{" "}
+          <strong>{user.attended_cp}</strong> {t("Campagne completate")}
+        </div>
+      </div>
+      <div>
+        <div>
+          <Title size="s">{t("Stato email")}</Title>
+        </div>
+        <div>
+          <Mailbox color={aqBootstrapTheme.palette.info} />{" "}
+          {t("Conferma l'email")}
+        </div>
+      </div>
+    </StyledHeaderProfile>
   );
 };
+
+const StyledHeaderProfile = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: ${(props) => props.theme.grid.spacing.default};
+  .profile-avatar {
+    width: 132px;
+    height: 132px;
+    border-radius: 50%;
+  }
+  @media (min-width: ${(props) => props.theme.grid.breakpoints.lg}) {
+    grid-template-columns: 20% 40% 40%;
+    .profile-avatar {
+      width: 100%;
+    }
+  }
+`;
