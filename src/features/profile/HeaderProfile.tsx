8859dc97d6facb2374f6913299aa12c4bccebc96
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-10-12 13:20:30 +0200

    feat: initial setup HeaderProfile.tsx

--- src/features/profile/HeaderProfile.tsx
+++ src/features/profile/HeaderProfile.tsx
@@ -1,6 +1,13 @@
 import { useTranslation } from "react-i18next";
 import UserStore from "../../redux/user";
-import { BSCol, BSGrid, PageTitle } from "@appquality/appquality-design-system";
+import {
+  BSCol,
+  BSGrid,
+  PageTitle,
+  Title,
+} from "@appquality/appquality-design-system";
+import { StarFill, BookmarkCheckFill, Mailbox } from "react-bootstrap-icons";
+import { aqBootstrapTheme } from "@appquality/appquality-design-system";
 
 export const HeaderProfile = () => {
   const { t } = useTranslation();
@@ -9,14 +16,30 @@ export const HeaderProfile = () => {
     <>
       <BSGrid>
         <BSCol size="col-3">
-          <img src={user.image}></img>
+          <img className="avatar" src={user.image}></img>
         </BSCol>
         <BSCol size="col-5">
           <PageTitle size="small" subtitle={"T" + user.id}>
-            {user.name}
+            {`${user.name} ${user.surname}`}
           </PageTitle>
+          <div>
+            <StarFill color={aqBootstrapTheme.palette.warning} />{" "}
+            <strong>{user.total_exp_pts}</strong> pt
+          </div>
+          <div>
+            <BookmarkCheckFill color={aqBootstrapTheme.palette.secondary} />{" "}
+            <strong>{user.attended_cp}</strong> {t("Campagne completate")}
+          </div>
         </BSCol>
-        <BSCol size="col-4"></BSCol>
+        <BSCol size="col-4">
+          <div>
+            <Title size="s">{t("Stato email")}</Title>
+          </div>
+          <div>
+            <Mailbox color={aqBootstrapTheme.palette.info} />{" "}
+            {t("Conferma l'email")}
+          </div>
+        </BSCol>
       </BSGrid>
     </>
   );
