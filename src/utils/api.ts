31361bf8356667c5a96779674d6d4a4891e2cb84
iacopolea <iacopo.lea@gmail.com>
2021-10-19 16:11:38 +0200

    feat(profile): add language api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,12 +2,14 @@ import * as authenticate from "./authenticate";
 import * as campaigns from "./campaigns";
 import * as user from "./users";
 import * as devices from "./devices";
+import * as languages from "./languages";
 
 const API = {
   ...authenticate,
   ...campaigns,
   ...user,
   ...devices,
+  ...languages,
 };
 
 export default API;
