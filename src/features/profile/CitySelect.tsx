30b7b09393517d082d5286bc167ee0e25dfd4a51
iacopolea <iacopo.lea@gmail.com>
2021-10-19 13:16:41 +0200

    feat(profile): add city select fetch call to geodb-cities

--- src/features/profile/CitySelect.tsx
+++ src/features/profile/CitySelect.tsx
@@ -6,10 +6,8 @@ import {
   SelectType,
 } from "@appquality/appquality-design-system";
 import { FieldProps, useFormikContext } from "formik";
-import { City } from "country-state-city";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { ICity } from "country-state-city/dist/lib/interface";
 import { Option } from "@appquality/appquality-design-system/dist/stories/select/_types";
 
 export const CitySelect = ({
@@ -22,33 +20,52 @@ export const CitySelect = ({
   countryCode?: string;
 }) => {
   const { values } = useFormikContext();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [filteredCities, setFilteredCities] = useState<SelectType.Option[]>([]);
   // const filteredCities = [{label:'',value:''}];
   useEffect(() => {
-    if (countryCode) {
-      let cities = City.getCitiesOfCountry(countryCode);
-      if (cities) {
-        let mappedCities = cities.map((city) => ({
-          label: city.name,
-          value: city.name,
-        }));
-        setFilteredCities(mappedCities);
+    const getCities = async (cCode: string) => {
+      const urlps = new URLSearchParams({
+        countryIds: cCode,
+        languageCode: i18n.language,
+      });
+
+      const params = "?" + urlps.toString();
+      try {
+        const res = await fetch(
+          "https://wft-geo-db.p.rapidapi.com/v1/geo/cities" + params,
+          {
+            method: "GET",
+            headers: {
+              "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+              "x-rapidapi-key":
+                "0b9310c254mshe16dee424f318b3p1d40b4jsn442890bc6db1",
+            },
+          }
+        );
+        if (!res.ok) throw new Error("buuuu");
+        const cities = await res.json();
+        setFilteredCities(
+          cities.data.map((city: { id: number; name: string }) => ({
+            label: city.name,
+            value: city.name,
+            id: city.id,
+          }))
+        );
+      } catch (err) {
+        // add error message
       }
-    }
+    };
+    if (!countryCode) return;
+    getCities(countryCode);
   }, [countryCode]);
-  useEffect(() => {
-    // @ts-ignore
-    console.log(values["country"]);
-  }, [values]);
   return (
     <FormikField name={name}>
       {({ field, form, meta }: FieldProps) => (
         <FormGroup>
-          <FormLabel htmlFor={field.name} label={label} />
           <Select
             name={field.name}
-            label={t("Domicile")}
+            label={label}
             value={field.value}
             isDisabled={form.values.country === ""}
             options={filteredCities}
