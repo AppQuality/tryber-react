9c2dafe2fb6a7218ed8eacb20b5d9bd44d30c297
iacopolea <iacopo.lea@gmail.com>
2021-10-20 12:04:10 +0200

    FIX(profile): remove wrong import

--- src/features/profile/CitySelect.tsx
+++ src/features/profile/CitySelect.tsx
@@ -9,7 +9,6 @@ import { FieldProps, useFormikContext } from "formik";
 import { useEffect, useMemo, useState } from "react";
 import sitewideMessageStore from "../../redux/siteWideMessages";
 import { useTranslation } from "react-i18next";
-import { Option } from "@appquality/appquality-design-system/dist/stories/select/_types";
 import HttpError from "../../utils/HttpError";
 import API from "../../utils/api";
 import countries, { LocalizedCountryNames } from "i18n-iso-countries";
