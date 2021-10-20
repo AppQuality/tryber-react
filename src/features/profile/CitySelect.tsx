63e7994d32b356f8d9a790b318ce16419027f690
iacopolea <iacopo.lea@gmail.com>
2021-10-20 17:38:01 +0200

    feat(profile): add city select async

--- src/features/profile/CitySelect.tsx
+++ src/features/profile/CitySelect.tsx
@@ -12,6 +12,7 @@ import { useTranslation } from "react-i18next";
 import HttpError from "../../utils/HttpError";
 import API from "../../utils/api";
 import countries, { LocalizedCountryNames } from "i18n-iso-countries";
+import { GeoDbCity } from "../../utils/geoDb";
 
 export const CitySelect = ({
   name,
@@ -26,32 +27,53 @@ export const CitySelect = ({
   const [filteredCities, setFilteredCities] = useState<SelectType.Option[]>([]);
   const enCountries = countries.getNames("en", { select: "official" });
 
-  useEffect(() => {
-    const getCities = async (cCode: string[]) => {
+  const getAsyncOptions = useMemo(() => {
+    return async (start: number, search?: string) => {
+      const limit = 100;
+      let options: SelectType.Option[] = [];
+      let more = false;
+      const countryCodes = Object.keys(enCountries).filter(
+        (key) => enCountries[key] === values.country
+      );
       try {
-        const results = await API.cities(cCode, i18n.language);
-        setFilteredCities(
-          results.data.map((city: { id: number; name: string }) => ({
-            label: city.name,
-            value: city.name,
-            id: city.id,
-          }))
+        const results = await API.cities({
+          countryIds: countryCodes,
+          languageCode: i18n.language,
+          offset: start,
+          search: search,
+          limit: limit,
+        });
+        const filteredCities = results.data.filter(
+          (res) => res.type === "CITY"
         );
-      } catch (err) {
-        const { message } = err as HttpError;
+        options = filteredCities.map((city: GeoDbCity) => ({
+          label: city.name,
+          value: city.name,
+          id: city.id,
+        }));
+        const { totalCount, currentOffset } = results.metadata;
+        more = !!(totalCount - (limit + currentOffset));
+      } catch (e) {
+        const { message, statusText } = e as HttpError;
         add({
           type: "danger",
           expire: false,
-          message: t("Error retrieving cities from GeoDb endpoint: ") + message,
+          message: `${t(
+            "Error retrieving cities from GeoDb endpoint: "
+          )} ${statusText} - ${message}`,
         });
       }
+      return new Promise<{ more: boolean; results: SelectType.Option[] }>(
+        (resolve, reject) => {
+          return resolve({
+            results: options,
+            more: more,
+          });
+        }
+      );
     };
-    if (!values.country) return;
-    const countryCodes = Object.keys(enCountries).filter(
-      (key) => enCountries[key] === values.country
-    );
-    getCities(countryCodes);
   }, [values.country]);
+
   return (
     <FormikField name={name}>
       {({ field, form, meta }: FieldProps) => (
@@ -59,11 +81,9 @@ export const CitySelect = ({
           <Select
             name={field.name}
             label={label}
-            value={filteredCities.filter(
-              (option) => option.value === field.value
-            )}
+            value={{ label: field.value, value: field.value }}
             isDisabled={form.values.country === ""}
-            options={filteredCities}
+            options={form.values.country ? getAsyncOptions : []}
             onBlur={() => {
               form.setFieldTouched(field.name);
             }}
