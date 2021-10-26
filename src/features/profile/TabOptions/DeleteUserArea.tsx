87521208f2ed824118fbafbe8e160e3b5004fc07
Davide Bizzi <davide.bizzi@app-quality.com>
2021-10-26 15:18:46 +0200

    fix(profile): Use mobile svg for settings

--- src/features/profile/TabOptions/DeleteUserArea.tsx
+++ src/features/profile/TabOptions/DeleteUserArea.tsx
@@ -7,7 +7,7 @@ import {
 } from "@appquality/appquality-design-system";
 import { useTranslation, Trans } from "react-i18next";
 import UserStore from "../../../redux/user";
-import leaveCrowd from "../assets/leave-crowd.png";
+import LeaveCrowd from "./LeaveCrowd";
 
 const DeleteUserArea = () => {
   const { t } = useTranslation();
@@ -18,34 +18,34 @@ const DeleteUserArea = () => {
       <BSCol size="col-12 col-lg-6">
         <Title size="xs" className="aq-mb-2">
           {t("Delete your account")}
-          <Text className="aq-mb-3">
-            <div>
-              <Trans
-                values={{ testerId: `T${user.id}` }}
-                i18nKey="The deletion of your account ({{testerId}}) <bold>will be irreversible</bold>.<br></br><br></br>You will not be able to continue earning money with us. <br></br>Are you sure you want to leave our community?"
-                defaults="The deletion of your account ({{testerId}}) <bold>will be irreversible</bold>.<br></br><br></br>You will not be able to continue earning money with us. <br></br>Are you sure you want to leave our community?"
-                components={{ br: <br />, bold: <strong /> }}
-              />
-            </div>
-          </Text>
-          <BSGrid>
-            <BSCol size="col-12 col-lg-6">
-              <Button
-                flat
-                size="block"
-                type="danger"
-                onClick={() => {
-                  openDeleteModal();
-                }}
-              >
-                {t("Delete account")}
-              </Button>
-            </BSCol>
-          </BSGrid>
         </Title>
+        <Text className="aq-mb-3">
+          <div>
+            <Trans
+              values={{ testerId: `T${user.id}` }}
+              i18nKey="The deletion of your account ({{testerId}}) <bold>will be irreversible</bold>.<br></br><br></br>You will not be able to continue earning money with us. <br></br>Are you sure you want to leave our community?"
+              defaults="The deletion of your account ({{testerId}}) <bold>will be irreversible</bold>.<br></br><br></br>You will not be able to continue earning money with us. <br></br>Are you sure you want to leave our community?"
+              components={{ br: <br />, bold: <strong /> }}
+            />
+          </div>
+        </Text>
+        <BSGrid>
+          <BSCol size="col-12 col-lg-6">
+            <Button
+              flat
+              size="block"
+              type="danger"
+              onClick={() => {
+                openDeleteModal();
+              }}
+            >
+              {t("Delete account")}
+            </Button>
+          </BSCol>
+        </BSGrid>
       </BSCol>
       <BSCol size="col-12 col-lg-6">
-        <img className="aq-float-right" src={leaveCrowd} />
+        <LeaveCrowd />
       </BSCol>
     </BSGrid>
   );
