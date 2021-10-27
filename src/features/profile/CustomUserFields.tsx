fec30b806b92570b3d336d77d6571acfd2d013a7
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-10-27 11:51:18 +0200

    feat(profile-advance): set initialUserValues and validationSchema for CustomUserFields.tsx

--- src/features/profile/CustomUserFields.tsx
+++ src/features/profile/CustomUserFields.tsx
@@ -10,16 +10,28 @@ import { FieldProps } from "formik";
 import API from "../../utils/api";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { array } from "yup";
 import { operations } from "../../utils/schema";
-export const CustomUserFields = ({ user }: UserData) => {
+import * as yup from "yup";
+import { shallowEqual, useSelector } from "react-redux";
+import { array } from "yup";
+
+export const CustomUserFields = () => {
+  const {
+    additional,
+  }: {
+    additional: UserData["additional"];
+  } = useSelector(
+    (state: GeneralState) => ({
+      additional: state.user.user?.additional,
+    }),
+    shallowEqual
+  );
   const [cufGroups, setCufGroups] = useState<
     operations["get-customUserFields"]["responses"]["200"]["content"]["application/json"]
   >([]);
-  const [cufOther, setCufOther] = useState<any>([]);
-  const [cufAccordions, setCufAccordions] = useState<any>([]);
   const { t } = useTranslation();
-  const userCuf = user?.addtional;
+  const initialUserValues: { [key: string]: string | object | object[] } = {};
+  const validationSchema: { [key: string]: yup.AnySchema } = {};
   useEffect(() => {
     const getCUF = async () => {
       const groups = await API.customUserFields({});
@@ -27,11 +39,56 @@ export const CustomUserFields = ({ user }: UserData) => {
     };
     getCUF();
   }, []);
+  cufGroups.forEach((group) => {
+    if (group.fields)
+      group.fields.forEach((field) => {
+        switch (field.type) {
+          case "multiselect":
+            const multiselectValue = additional?.filter(
+              (cuf: UserData["additional"][0]) => cuf.field_id === field.id
+            );
+            validationSchema["cuf_" + field.id] = yup.array(yup.object());
+            initialUserValues["cuf_" + field.id] = multiselectValue
+              ? multiselectValue
+              : [];
+            break;
+          case "select":
+            const selectValue = additional.find(
+              (cuf: UserData["additional"][0]) => cuf.field_id === field.id
+            );
+            validationSchema["cuf_" + field.id] = yup.object();
+            initialUserValues["cuf_" + field.id] = selectValue
+              ? selectValue
+              : {};
+            break;
+          case "text":
+            const textValue = additional.find(
+              (cuf: UserData["additional"][0]) => cuf.field_id === field.id
+            );
+            initialUserValues["cuf_" + field.id] = textValue
+              ? textValue.value
+              : "";
+            const formatData = field.format ? field.format.split(";") : false;
+            const format = formatData ? new RegExp(formatData[0]) : false;
+            const formatMessage =
+              formatData && formatData.length > 1
+                ? formatData[1]
+                : t("Invalid format");
+            validationSchema["cuf_" + field.id] = format
+              ? yup.string().matches(format, formatMessage)
+              : yup.string();
+            break;
+        }
+      });
+  });
+  console.log(initialUserValues);
+  console.log(validationSchema);
   const groupOfFieldsWithoutGroup = cufGroups.find((g) => g.group.id === 0);
   let fieldsWithoutGroup: typeof cufGroups[0]["fields"] = [];
   if (groupOfFieldsWithoutGroup) {
     fieldsWithoutGroup = groupOfFieldsWithoutGroup.fields;
   }
+
   const groupsOfFields = cufGroups.filter((g) => g.group.id !== 0);
   return (
     <>
@@ -48,7 +105,7 @@ export const CustomUserFields = ({ user }: UserData) => {
           />
         ))}
 
-      <Accordion initialActive="item1">
+      <Accordion initialActive="">
         {groupsOfFields &&
           groupsOfFields.map(
             (cufGroup) =>
