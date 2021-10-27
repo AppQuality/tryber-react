f292c381880f4e27ddca1d6553fbea2f946f94c6
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-10-27 14:51:08 +0200

    feat(profile-advance): add CufField.tsx

--- src/features/profile/TabAdvanced/CustomUserFields.tsx
+++ src/features/profile/TabAdvanced/CustomUserFields.tsx
@@ -1,19 +1,16 @@
 import {
   Field,
-  FormGroup,
-  FormikField,
-  Select,
-  SelectType,
   Accordion,
+  Formik,
+  Form,
 } from "@appquality/appquality-design-system";
-import { FieldProps } from "formik";
-import API from "../../utils/api";
+import API from "../../../utils/api";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { operations } from "../../utils/schema";
+import { operations } from "../../../utils/schema";
 import * as yup from "yup";
 import { shallowEqual, useSelector } from "react-redux";
-import { array } from "yup";
+import CufField from "./CufField";
 
 export const CustomUserFields = () => {
   const {
@@ -34,8 +31,12 @@ export const CustomUserFields = () => {
   const validationSchema: { [key: string]: yup.AnySchema } = {};
   useEffect(() => {
     const getCUF = async () => {
-      const groups = await API.customUserFields({});
-      setCufGroups(groups);
+      try {
+        const groups = await API.customUserFields({});
+        setCufGroups(groups);
+      } catch (e) {
+        console.log(e);
+      }
     };
     getCUF();
   }, []);
@@ -91,39 +92,42 @@ export const CustomUserFields = () => {
 
   const groupsOfFields = cufGroups.filter((g) => g.group.id !== 0);
   return (
-    <>
-      {/*foreach Others CUF add field input*/}
-      {fieldsWithoutGroup &&
-        fieldsWithoutGroup.map((field) => (
-          // <CufField key... type={field.type}
-          <Field
-            key={field.id}
-            name={`cufOther.${field.id}`}
-            type="text"
-            label={field.placeholder?.it}
-            placeholder={field.placeholder?.it}
-          />
-        ))}
+    <Formik
+      enableReinitialize
+      initialValues={initialUserValues}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form>
+        {/*foreach Others CUF add field input*/}
+        {fieldsWithoutGroup &&
+          fieldsWithoutGroup.map((field) => (
+            <CufField cufField={field} key={`cuf_${field.id}`} />
+          ))}
 
-      <Accordion initialActive="">
-        {groupsOfFields &&
-          groupsOfFields.map(
-            (cufGroup) =>
-              // create an accordion Item forach
-              cufGroup.fields && (
-                <Accordion.Item
-                  id={cufGroup.group.id.toString()}
-                  title={<h4>{cufGroup.group.name.it}</h4>}
-                >
-                  {/*cufGroups[index] = fields (foreach fields check the type )*/}
-                  {/*the type can be SELECT || MULTI-SELECT*/}
-                  {cufGroup.fields.map((field) => {
-                    return <p>{field.type}</p>;
-                  })}
-                </Accordion.Item>
-              )
-          )}
-      </Accordion>
-    </>
+        <Accordion initialActive="">
+          {groupsOfFields &&
+            groupsOfFields.map(
+              (cufGroup) =>
+                // create an accordion Item forach
+                cufGroup.fields && (
+                  <Accordion.Item
+                    id={cufGroup.group.id.toString()}
+                    title={<h4>{cufGroup.group.name.it}</h4>}
+                  >
+                    {/*cufGroups[index] = fields (foreach fields check the type )*/}
+                    {/*the type can be SELECT || MULTI-SELECT*/}
+                    {cufGroup.fields.map((field) => {
+                      return (
+                        <CufField cufField={field} key={`cuf_${field.id}`} />
+                      );
+                    })}
+                  </Accordion.Item>
+                )
+            )}
+        </Accordion>
+      </Form>
+    </Formik>
   );
 };
