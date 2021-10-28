fcc93ca14a1d98cb1a010c268acf64a901fbc950
iacopolea <iacopo.lea@gmail.com>
2021-10-28 12:47:21 +0200

    feat(profile): tab advanced show errors

--- src/features/profile/TabAdvanced/index.tsx
+++ src/features/profile/TabAdvanced/index.tsx
@@ -27,39 +27,50 @@ export const TabAdvanced = () => {
     <Formik
       enableReinitialize
       initialValues={initialUserValues}
-      validationSchema={validationSchema}
+      validationSchema={yup.object(validationSchema)}
       onSubmit={(values) => {
         console.log(values);
       }}
     >
-      <Form id="advancedProfileForm">
-        <CSSGrid gutter="50px" rowGap="1rem" min="220px" className="aq-m-3">
-          <div className="employment">
-            <Title size="s">{t("Employment")}</Title>
-            <EmploymentSelect name="employment" label={t("Profession")} />
-            <EducationSelect name="education" label={t("Education level")} />
-            <Title size="xs">{t("Certifications")}</Title>
-            <Certifications />
-          </div>
-          <div className="address">
-            <Title size="s">{t("Additional fields")}</Title>
-            <Text>
-              {t(
-                "Improve your chances of being selected in test campaigns by completing your profile."
+      {(formikProps) => (
+        <Form id="advancedProfileForm">
+          <CSSGrid gutter="50px" rowGap="1rem" min="220px" className="aq-m-3">
+            <div className="employment">
+              <Title size="s">{t("Employment")}</Title>
+              <EmploymentSelect name="employment" label={t("Profession")} />
+              <EducationSelect name="education" label={t("Education level")} />
+              <Title size="xs">{t("Certifications")}</Title>
+              <Certifications />
+            </div>
+            <div className="address">
+              <Title size="s">{t("Additional fields")}</Title>
+              <Text>
+                {t(
+                  "Improve your chances of being selected in test campaigns by completing your profile."
+                )}
+              </Text>
+              <CustomUserFields />
+              {formikProps.errors && (
+                <Text color="danger" small>
+                  <ul style={{ listStyle: "disc" }}>
+                    {Object.entries(formikProps.errors).map((value, key) => (
+                      <li>{value}</li>
+                    ))}
+                  </ul>
+                </Text>
               )}
-            </Text>
-            <CustomUserFields />
-            <Button
-              type="success"
-              htmlType="submit"
-              flat={true}
-              disabled={false}
-            >
-              {t("Save")}
-            </Button>
-          </div>
-        </CSSGrid>
-      </Form>
+              <Button
+                type="success"
+                htmlType="submit"
+                flat={true}
+                disabled={false}
+              >
+                {t("Save")}
+              </Button>
+            </div>
+          </CSSGrid>
+        </Form>
+      )}
     </Formik>
   );
 };
