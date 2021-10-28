74b58a9e4408f66f78fea258f17365febff6f007
iacopolea <iacopo.lea@gmail.com>
2021-10-28 16:20:24 +0200

    wip(profile): tab advanced types init

--- src/features/profile/TabAdvanced/Certifications.tsx
+++ src/features/profile/TabAdvanced/Certifications.tsx
@@ -1,27 +1,55 @@
-import { Radio, Button } from "@appquality/appquality-design-system";
+import {
+  Radio,
+  Button,
+  FormGroup,
+  FormikField,
+  ErrorMessage,
+} from "@appquality/appquality-design-system";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useFormikContext } from "formik";
+import { FieldProps, useFormikContext } from "formik";
 import modalStore from "../../../redux/modal";
 import { NewCertificationModal } from "./NewCertificationModal";
 
 const Certifications = () => {
   const { t } = useTranslation();
-  const { values } = useFormikContext<{ certifications: true }>();
+  const { values } = useFormikContext<AdvancedFormValues>();
   const { open } = modalStore();
   return (
     <>
-      <Radio
-        name="certifications"
-        id="noCertifications"
-        label={t("I have no certifications")}
-      ></Radio>
-      <Radio
-        name="certifications"
-        id="yesCertifications"
-        label={t("I have the certifications")}
-      ></Radio>
-      {values.certifications && (
+      <FormikField name="certificationsRadio">
+        {({
+          field, // { name, value, onChange, onBlur }
+          form,
+        }: FieldProps) => (
+          <FormGroup>
+            <Radio
+              value="false"
+              name={field.name}
+              checked={field.value === "false"}
+              id="certificationFalse"
+              label={t("I have no certifications")}
+              onChange={(v) => {
+                form.setFieldTouched(field.name);
+                form.setFieldValue(field.name, v);
+              }}
+            />
+            <Radio
+              value="true"
+              name={field.name}
+              checked={field.value === "true"}
+              id="certificationTrue"
+              label={t("I have the certifications")}
+              onChange={(v) => {
+                form.setFieldTouched(field.name);
+                form.setFieldValue(field.name, v);
+              }}
+            />
+            <ErrorMessage name={field.name} />
+          </FormGroup>
+        )}
+      </FormikField>
+      {values.certificationsRadio && (
         <Button
           type="link"
           htmlType="button"
