f33093941cbc39d3fc5f8dc943f7fb9385822280
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-11-02 10:44:43 +0100

    feat: add title to generic modal

--- src/features/GenericModal.tsx
+++ src/features/GenericModal.tsx
@@ -2,10 +2,15 @@ import modalStore from "../redux/modal";
 import { Modal } from "@appquality/appquality-design-system";
 
 const GenericModal = () => {
-  const { isOpen, close, content, footer } = modalStore();
+  const { isOpen, close, content, footer, title } = modalStore();
 
   return (
-    <Modal isOpen={isOpen} footer={footer} onClose={() => close()}>
+    <Modal
+      isOpen={isOpen}
+      footer={footer}
+      title={title}
+      onClose={() => close()}
+    >
       {content}
     </Modal>
   );
