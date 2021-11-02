635395f21c9aa32a0d27a5e920716363f6bdf4da
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-11-02 10:40:29 +0100

    feat: add title to redux modal

--- src/redux/modal/index.ts
+++ src/redux/modal/index.ts
@@ -7,15 +7,21 @@ export default () => {
     open,
     content,
     footer,
-  }: { open: boolean; content?: React.ReactNode; footer?: React.ReactNode } =
-    useSelector(
-      (state: GeneralState) => ({
-        open: state.modal.open,
-        content: state.modal.content,
-        footer: state.modal.footer,
-      }),
-      shallowEqual
-    );
+    title,
+  }: {
+    open: boolean;
+    content?: React.ReactNode;
+    footer?: React.ReactNode;
+    title?: React.ReactNode;
+  } = useSelector(
+    (state: GeneralState) => ({
+      open: state.modal.open,
+      content: state.modal.content,
+      footer: state.modal.footer,
+      title: state.modal.title,
+    }),
+    shallowEqual
+  );
 
   const dispatch: Dispatch<any> = useDispatch();
 
@@ -23,13 +29,16 @@ export default () => {
     isOpen: open,
     content: content,
     footer: footer,
+    title: title,
     close: () => dispatch(closeModal()),
     open: ({
       content,
       footer,
+      title,
     }: {
       content?: React.ReactNode;
       footer?: React.ReactNode;
-    }) => dispatch(openModal(content, footer)),
+      title?: React.ReactNode;
+    }) => dispatch(openModal(content, footer, title)),
   };
 };
