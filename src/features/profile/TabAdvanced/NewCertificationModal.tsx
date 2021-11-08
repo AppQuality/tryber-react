2ca1cd2f71a11d8ddbaa388b16c95fc702791b56
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2021-11-08 11:33:08 +0100

    wip(advanced-profile)certification refactor

--- src/features/profile/TabAdvanced/NewCertificationModal.tsx
+++ src/features/profile/TabAdvanced/NewCertificationModal.tsx
@@ -2,13 +2,27 @@ import { Formik } from "@appquality/appquality-design-system";
 import { FormikProps } from "formik";
 import { CertificationFields } from "../types";
 import NewCertificationModalForm from "./NewCertificationModalForm";
+import API from "../../../utils/api";
+import userStore from "../../../redux/user";
 
 export const NewCertificationModal = () => {
+  const { refresh } = userStore();
   return (
     <Formik
       initialValues={{ institute: "", area: "", certificationId: "" }}
       onSubmit={(values) => {
         console.log(values);
+        API.addCertification({
+          certification_id: parseInt(values.certificationId),
+          achievement_date: "2021-11-10",
+        })
+          .then(() => {
+            refresh();
+            //TODO: ADD good event message
+          })
+          .catch((e) => {
+            console.log(e);
+          });
       }}
     >
       {(formikProps: FormikProps<CertificationFields>) => {
--- src/utils/certifications/index.ts
+++ src/utils/certifications/index.ts
@@ -4,7 +4,7 @@ import apifetch from "../apifetch";
 export const certifications = async ({
   filterBy,
 }: {
-  filterBy?: { institute?: string; area?: string };
+  filterBy?: operations["get-certifications"]["parameters"]["query"]["filterBy"];
 }): Promise<
   operations["get-certifications"]["responses"]["200"]["content"]["application/json"]
 > => {
@@ -12,5 +12,18 @@ export const certifications = async ({
     endpoint: "/certifications",
     params: filterBy,
     paramType: "filterBy",
+  });
+};
+
+export const addCertification = async (certification: {
+  certification_id: number;
+  achievement_date: string;
+}): Promise<
+  operations["get-certifications"]["responses"]["200"]["content"]["application/json"]
+> => {
+  return apifetch({
+    endpoint: "/users/me/certifications",
+    method: "POST",
+    body: certification,
   });
 };
