14a4487a37b30bd4aa53e12d63521ee45f513d65
Davide Bizzi <davide.bizzi@app-quality.com>
2021-11-09 16:17:08 +0100

    fix(fiscal): Send toastr on fiscal profile update

--- src/redux/siteWideMessages/actionCreators.ts
+++ src/redux/siteWideMessages/actionCreators.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { aqBootstrapTheme } from "@appquality/appquality-design-system";
 
 export function addMessage(
-  message: string,
+  message: React.ReactNode,
   type: keyof typeof aqBootstrapTheme.palette,
   expire: number | false = 3
 ) {
--- src/redux/user/actions/updateFiscalProfile.ts
+++ src/redux/user/actions/updateFiscalProfile.ts
@@ -1,27 +1,45 @@
 import * as actionTypes from "../actionTypes";
 import API from "../../../utils/api";
 import { operations } from "../../../utils/schema";
+import { addMessage } from "../../siteWideMessages/actionCreators";
 
 export const updateFiscalProfile = (
   data:
     | operations["post-users-me-fiscal"]["requestBody"]["content"]["application/json"]
-    | operations["put-users-me-fiscal"]["requestBody"]["content"]["application/json"]
+    | operations["put-users-me-fiscal"]["requestBody"]["content"]["application/json"],
+  {
+    verifiedMessage = "Verified",
+    unverifiedMessage = "Unverified",
+  }: {
+    verifiedMessage?: React.ReactNode;
+    unverifiedMessage?: React.ReactNode;
+  } = {}
 ) => {
-  return async (dispatch: UserDispatchType, getState: () => GeneralState) => {
+  return async (
+    dispatch: UserDispatchType | SiteWideMessagesDispatchType,
+    getState: () => GeneralState
+  ) => {
+    const userDispatch = dispatch as UserDispatchType;
+    const messageDispatch = dispatch as any;
     const state = getState();
     const { user } = state;
-    dispatch({ type: actionTypes.FETCH_FISCAL_PROFILE_LOADING });
+    userDispatch({ type: actionTypes.FETCH_FISCAL_PROFILE_LOADING });
     try {
       const newFiscalData = user.user.fiscal
         ? await API.putFiscalData({ data: data })
         : await API.postFiscalData({ data: data });
-      dispatch({
+      userDispatch({
         type: actionTypes.FETCH_FISCAL_PROFILE,
         data: { ...newFiscalData },
       });
+      if (newFiscalData.fiscalStatus === "Verified") {
+        messageDispatch(addMessage(verifiedMessage, "success"));
+      } else {
+        messageDispatch(addMessage(unverifiedMessage, "danger"));
+      }
     } catch (err: unknown) {
       const { statusCode } = err as HttpError;
-      dispatch({
+      userDispatch({
         type: actionTypes.FETCH_FISCAL_PROFILE_FAILED,
         error: err as HttpError,
       });
