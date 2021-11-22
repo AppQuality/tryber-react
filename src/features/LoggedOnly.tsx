47d124e58ad2e2905f9b97c9dd907d15be4ffd1b
iacopolea <iacopo.lea@gmail.com>
2021-11-22 15:29:17 +0100

    test(loading): test animated loading svg

--- src/features/LoggedOnly.tsx
+++ src/features/LoggedOnly.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { shallowEqual, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useLocalizeRoute } from "../hooks/useLocalizedRoute";
+import LoadingSvg from "src/pages/assets/loading.svg";
 
 export default ({ children }: { children: React.ReactNode }) => {
   const history = useHistory();
@@ -19,13 +20,14 @@ export default ({ children }: { children: React.ReactNode }) => {
     }),
     shallowEqual
   );
-  const { t } = useTranslation();
 
   if (loading || typeof loading === "undefined") {
     return (
       <div id="appq-loading-content">
-        <div id="appq-loading-spinner">
-          <div></div>
+        <div style={{ width: "60px", height: "60px" }}>
+          <object type="image/svg+xml" data={LoadingSvg}>
+            svg-animation
+          </object>
         </div>
       </div>
     );
