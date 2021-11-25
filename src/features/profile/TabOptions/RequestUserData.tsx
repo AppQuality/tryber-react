dc72c2aa46f0f4952efeda602d995fde2fc2856c
iacopolea <iacopo.lea@gmail.com>
2021-11-25 11:12:57 +0100

    fix(profile): add half column button

--- src/features/profile/TabOptions/RequestUserData.tsx
+++ src/features/profile/TabOptions/RequestUserData.tsx
@@ -1,13 +1,8 @@
-import {
-  Button,
-  Text,
-  Title,
-  BSGrid,
-  BSCol,
-} from "@appquality/appquality-design-system";
+import { Text, CSSGrid, Title } from "@appquality/appquality-design-system";
 import { useTranslation } from "react-i18next";
 import siteWideMessageStore from "../../../redux/siteWideMessages";
 import WPAPI from "../../../utils/wpapi";
+import { HalfColumnButton } from "src/pages/profile/HalfColumnButton";
 
 const RequestUserData = () => {
   const { t, i18n } = useTranslation();
@@ -22,43 +17,41 @@ const RequestUserData = () => {
           "Your data are safe with us. You can ask them anytime you need. Upon your request, we will provide you a copy of your personal data at the email associated to your Tester ID."
         )}
       </Text>
-      <BSGrid>
-        <BSCol size="col-6">
-          <Button
-            type="primary"
-            size="block"
-            htmlType="submit"
-            flat={true}
-            onClick={async () => {
-              WPAPI.requestUserData(i18n.language)
-                .then(() => {
-                  add({
-                    message: t(
-                      "We've received your request. You'll get an email in a few days."
-                    ),
-                    type: "success",
-                  });
-                })
-                .catch((e) => {
-                  const { message } = e as HttpError;
-                  if (message === "GENERIC_ERROR") {
-                    add({
-                      message: t("There was an error"),
-                      type: "danger",
-                    });
-                  } else {
-                    add({
-                      message: message,
-                      type: "danger",
-                    });
-                  }
+      <CSSGrid min="50%" gutter="0" fill="true">
+        <HalfColumnButton
+          type="primary"
+          size="block"
+          htmlType="submit"
+          flat={true}
+          onClick={async () => {
+            WPAPI.requestUserData(i18n.language)
+              .then(() => {
+                add({
+                  message: t(
+                    "We've received your request. You'll get an email in a few days."
+                  ),
+                  type: "success",
                 });
-            }}
-          >
-            {t("Request data")}
-          </Button>
-        </BSCol>
-      </BSGrid>
+              })
+              .catch((e) => {
+                const { message } = e as HttpError;
+                if (message === "GENERIC_ERROR") {
+                  add({
+                    message: t("There was an error"),
+                    type: "danger",
+                  });
+                } else {
+                  add({
+                    message: message,
+                    type: "danger",
+                  });
+                }
+              });
+          }}
+        >
+          {t("Request data")}
+        </HalfColumnButton>
+      </CSSGrid>
     </div>
   );
 };
