68d450e5c8909213913321c77f4aefb8bd6f803b
iacopolea <iacopo.lea@gmail.com>
2022-01-10 16:31:15 +0100

    wip(home): change font and images. Remove shapes

--- src/pages/home/content/BannerTop.tsx
+++ src/pages/home/content/BannerTop.tsx
@@ -6,86 +6,22 @@ import {
   Title,
 } from "@appquality/appquality-design-system";
 import React, { useEffect, useRef, useState } from "react";
-import { ReactComponent as TopShape } from "../assets/rectangle-top.svg";
-import people from "../assets/join-the-community.svg";
+import people from "../assets/tryber_home_welcome.png";
 import { LangMenu } from "../../../features/LangMenu";
 import styled from "styled-components";
-import { CSSTransition } from "react-transition-group";
 import { Trans, useTranslation } from "react-i18next";
 import JoinTheTeamButton from "./JoinTheTeamButton";
 
-const TopAnimation = styled.div`
-  display: none;
-  @media (min-width: ${(props) => props.theme.grid.breakpoints.lg}) {
-    display: block;
-  }
-  position: relative;
-  height: 500px;
-
-  svg {
-    position: absolute;
-    top: 0;
-    left: 0;
-    will-change: transform;
-    @media (min-width: ${(props) => props.theme.grid.breakpoints.xl}) {
-      top: 25px;
-    }
-  }
-  img {
-    position: absolute;
-    top: 110px;
-    right: -50px;
-  }
-  .shape-animation-container {
-    position: relative;
-    will-change: transform;
-    transition: all 500ms ease-in-out;
-    transform: translate3d(200%, 0, 0);
-  }
-  .shape-animation-enter.shape-animation-enter-active {
-    transform: translate3d(0, 0, 0);
-  }
-  .shape-animation-enter-done {
-    transform: translate3d(0, 0, 0);
-  }
-`;
-
 export const BannerTop = () => {
   const { t } = useTranslation();
-  const [entry, setEntry] = useState<IntersectionObserverEntry>();
-  const [bottomDistance, setBottomDistance] = useState(0);
-  const [shapeIsVisible, setShapeVisible] = useState(false);
-  let containerRef = useRef<HTMLDivElement>(null);
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1.0,
-  };
-  const callBack: IntersectionObserverCallback = (entries) => {
-    if (entries[0]) setEntry(entries[0]);
-  };
-  useEffect(() => {
-    const observer = new IntersectionObserver(callBack, options);
-    if (containerRef.current) observer.observe(containerRef.current);
-    return () => {
-      if (containerRef.current) observer.unobserve(containerRef.current);
-    };
-  }, [containerRef, options]);
-  useEffect(() => {
-    setBottomDistance(entry?.boundingClientRect?.bottom || 0);
-  }, [entry]);
-  useEffect(() => {
-    setTimeout(() => {
-      setShapeVisible(true);
-    }, 300);
-  }, []);
+
   return (
     <BSGrid>
       <LangMenu className="aq-mb-3" itLink="/it" enLink="/" esLink="/es" />
       <BSCol size="col-lg-7 col-xxl-8">
         <div style={{ position: "relative" }}>
           <PageTitle>
-            <Title size="xl">
+            <Title size="xl" color="secondary" className="text-marker">
               {t("Join the AppQuality Tester Community!")}
             </Title>
           </PageTitle>
@@ -114,25 +50,7 @@ export const BannerTop = () => {
         </div>
       </BSCol>
       <BSCol size="col-lg-5 col-xxl-4">
-        <TopAnimation ref={containerRef}>
-          <CSSTransition
-            in={shapeIsVisible}
-            key="shape-animation"
-            timeout={500}
-            classNames="shape-animation"
-          >
-            <div className="shape-animation-container">
-              <TopShape
-                className="top-shape"
-                style={{
-                  transform: `translate3d(0, ${bottomDistance - 607}px, 0)
-                          rotate(calc(${bottomDistance - 607}deg * -0.1)`,
-                }}
-              />
-            </div>
-          </CSSTransition>
-          <img className="top-image" src={people} />
-        </TopAnimation>
+        <img src={people} alt="tryber welcome people" />
       </BSCol>
     </BSGrid>
   );
