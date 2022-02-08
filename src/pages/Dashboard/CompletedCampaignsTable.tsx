b639e30f820f9a326626cca08cd4fda4b455ce65
iacopolea <iacopo.lea@gmail.com>
2022-02-08 18:42:57 +0100

    feat(dashboard): sort completed campaigns mobile select

--- src/pages/Dashboard/CompletedCampaignsTable.tsx
+++ src/pages/Dashboard/CompletedCampaignsTable.tsx
@@ -1,13 +1,50 @@
-import { Pagination, Table } from "@appquality/appquality-design-system";
+import {
+  Pagination,
+  Table,
+  TableType,
+} from "@appquality/appquality-design-system";
 import { useTranslation } from "react-i18next";
 import useCompletedCampaigns from "./effects/useCompletedCampaigns";
+import { SortTableSelect } from "src/features/SortTableSelect";
 
 const CompletedCampaignsTable = () => {
   const { t } = useTranslation();
   const { campaigns, page, totalEntries, limit, loading, order, orderBy } =
     useCompletedCampaigns();
+  const columns: TableType.Column[] = [
+    {
+      title: t("Campaign"),
+      dataIndex: "campaigns",
+      key: "campaigns",
+      role: "title",
+      hideIndex: true,
+    },
+    {
+      title: t("End Date"),
+      dataIndex: "endDate",
+      key: "endDate",
+      isSortable: true,
+      onSort: (sorting: "ASC" | "DESC") => {
+        order.set(sorting);
+        orderBy.set("endDate");
+      },
+    },
+    {
+      title: t("Action"),
+      dataIndex: "actions",
+      key: "actions",
+      align: "center",
+      role: "cta",
+      hideIndex: true,
+    },
+  ];
   return (
     <>
+      <SortTableSelect
+        order={order.current}
+        orderBy={orderBy.current}
+        columns={columns}
+      />
       <Table
         dataSource={campaigns}
         isLoading={loading}
@@ -20,33 +57,7 @@ const CompletedCampaignsTable = () => {
             "There are currently no completed campaigns. You can relax, finish the active ones or apply in the new available campaigns!"
           ),
         }}
-        columns={[
-          {
-            title: t("Campaign"),
-            dataIndex: "campaigns",
-            key: "campaigns",
-            role: "title",
-            hideIndex: true,
-          },
-          {
-            title: t("End Date"),
-            dataIndex: "endDate",
-            key: "endDate",
-            isSortable: true,
-            onSort: (sorting: "ASC" | "DESC") => {
-              order.set(sorting);
-              orderBy.set("endDate");
-            },
-          },
-          {
-            title: t("Action"),
-            dataIndex: "actions",
-            key: "actions",
-            align: "center",
-            role: "cta",
-            hideIndex: true,
-          },
-        ]}
+        columns={columns}
       />
       {totalEntries > limit ? (
         <Pagination
