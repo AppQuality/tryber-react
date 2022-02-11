46e46ed6de5908763a9a17f7db5f330edb4c4046
Davide Bizzi <davide.bizzi@app-quality.com>
2022-02-11 17:00:44 +0100

    feat(home): Rework footer

--- src/pages/home/Footer/index.tsx
+++ src/pages/home/Footer/index.tsx
@@ -1,83 +1,91 @@
-import { Button, Text, Title } from '@appquality/appquality-design-system';
-import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
+import { Button, Text, Title } from "@appquality/appquality-design-system";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 
-import fbLogo from './assets/fb.svg';
-import instaLogo from './assets/insta.svg';
-import telegramLogo from './assets/teleg.svg';
-import logo from './assets/tryber_logo_full.svg';
+import fbLogo from "./assets/fb.svg";
+import instaLogo from "./assets/insta.svg";
+import telegramLogo from "./assets/teleg.svg";
+import { ReactComponent as Logo } from "./assets/tryber_logo_full.svg";
 
-const StyledFooter = styled.div`
+const FooterContainer = styled.div`
+  border-top: 1px solid ${(props) => props.theme.palette.primary};
+  padding: 0px 20px;
+
   ${Title} {
     text-transform: capitalize;
-    margin-bottom: ${(props) => props.theme.grid.spacing.default};
   }
-  @media (min-width: ${(props) => props.theme.grid.breakpoints.lg}) {
+  .grid-item {
+    margin: 12px 0;
+  }
+  .logo {
+    padding: 42px;
+  }
+  @media (min-width: ${(props) => props.theme.grid.breakpoints.md}) {
+    padding-top: 60px;
     display: grid;
-    grid-template-columns: 210px 140px 1fr 1.1fr;
-    grid-gap: 60px;
-  }
-  .trb-footer-logo {
-    margin-left: auto;
-    margin-right: auto;
-    display: block;
-    @media (min-width: ${(props) => props.theme.grid.breakpoints.lg}) {
-      display: initial;
+    grid-column-gap: 80px;
+    grid-row-gap: 0px;
+
+    .logo {
+      padding: 0;
+      margin: 0 auto;
+      grid-area: logo;
+      svg {
+        width: 100%;
+      }
     }
-  }
-  .social-btn {
-    padding: 0;
-    font-size: 28px;
-    height: 28px;
-    width: 28px;
-    display: inline-block;
-    border-radius: 4px;
-    overflow: hidden;
-    margin-right: ${(props) => props.theme.grid.spacing.default};
-    img {
-      width: 100%;
-      height: 100%;
-      display: block;
-      object-fit: cover;
+    .info {
+      grid-area: info;
     }
+    .address {
+      grid-area: address;
+    }
+    .social {
+      grid-area: social;
+    }
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-areas:
+      "logo info a"
+      "b address social";
+  }
+
+  @media (min-width: ${(props) => props.theme.grid.breakpoints.lg}) {
+    text-align: left;
+    margin-left: 80px;
+    margin-left: 80px;
   }
 `;
 
 export default () => {
   const { t } = useTranslation();
   return (
-    <StyledFooter>
-      <a href="https://tryber.me">
-        <img className="trb-footer-logo aq-mb-3" src={logo} />
-      </a>
-      <div className="aq-mb-3">
-        <Title size="s">{t("company")}</Title>
-        <Text small className="aq-mb-2">
-          <div style={{ display: "flex" }}>
-            APP QUALITY S.R.L. - P.IVA 01603290196
-          </div>
-        </Text>
-        <Text small className="aq-mb-2">
-          <div style={{ display: "flex" }}>
-            Capitale Sociale € 22.060,57, di cui € 19.174,11 i.v
-          </div>
-        </Text>
+    <FooterContainer>
+      <div className="grid-item logo">
+        <Logo />
       </div>
-      <div className="aq-mb-3">
-        <Title size="s">{t("address")}</Title>
-        <Text small className="aq-mb-2">
-          <div style={{ display: "flex" }}>
-            Via del Chiesotto 4, 26100 Cremona, Italy
-          </div>
-        </Text>
+      <div className="grid-item info">
+        <Title size="ms" className="aq-mb-3">
+          {t("company")}
+        </Title>
         <Text small>
-          <div style={{ display: "flex" }}>
-            <a href="mailto:support@tryber.me">support@tryber.me</a>
-          </div>
+          AppQuality srl - P.IVA 01603290196 <br />
+          Capitale Sociale € 22.060,57, di cui € 19.174,11 i.v
         </Text>
       </div>
-      <div className="aq-mb-3">
-        <Title size="s">{t("social")}</Title>
+      <div className="grid-item address">
+        <Title size="ms" className="aq-mb-3">
+          {t("address")}
+        </Title>
+        <Text small>
+          Via del Chiesotto 4, 26100 Cremona, Italy <br />
+          support@tryber.me
+        </Text>
+      </div>
+      <div className="grid-item social">
+        <Title size="ms" className="aq-mb-3">
+          {t("social")}
+        </Title>
         <Button
           forwardedAs="a"
           href="https://www.facebook.com/tryber.me"
@@ -106,6 +114,6 @@ export default () => {
           <img src={telegramLogo} alt="telegram" />
         </Button>
       </div>
-    </StyledFooter>
+    </FooterContainer>
   );
 };
