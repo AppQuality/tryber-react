2db204a45aaa78391664bb80d42a67424ce7658b
Davide Bizzi <davide.bizzi@app-quality.com>
2022-02-14 11:16:09 +0100

    fix(home): Fix tryber position in unguess area

--- src/pages/home/content/AboutUnguess/index.tsx
+++ src/pages/home/content/AboutUnguess/index.tsx
@@ -2,7 +2,7 @@ import { Button, Text, Title } from "@appquality/appquality-design-system";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 
-import tryber from "./assets/tryber.png";
+import { ReactComponent as Tryber } from "./assets/tryber.svg";
 
 const SlideContainer = styled.div`
   margin-top: 160px;
@@ -25,9 +25,19 @@ const SlideContainer = styled.div`
     grid-template-rows: 1fr 1fr;
     grid-template-areas: "left" "right";
   }
+  .tryber {
+    display: none;
+    @media (min-width: ${(props) => props.theme.grid.breakpoints.md}) {
+      display: block;
+    }
+  }
 `;
 const TextContainer = styled.div`
   padding: 16px 26px 110px 26px;
+  ${Title} {
+    display: flex;
+    align-items: center;
+  }
 `;
 const VideoContainer = styled.div`
   position: relative;
@@ -35,11 +45,6 @@ const VideoContainer = styled.div`
     border-radius: 21px;
     width: 100%;
     height: 100%;
-  }
-  img.tryber {
-    position: absolute;
-    top: -30%;
-    right: -5%;
   }
 `;
 
@@ -49,7 +54,10 @@ export const AboutUnguess = () => {
     <SlideContainer>
       <TextContainer>
         <Title size="xl" className="text-marker">
-          {t("__HOME_TITLE_UNGUESS MAX:40")}
+          <div>{t("__HOME_TITLE_UNGUESS MAX:40")}</div>
+          <div>
+            <Tryber className="tryber" />
+          </div>
         </Title>
         <Text className="aq-my-4">{t("__HOME_PARAGRAPH_UNGUESS MAX:120")}</Text>
       </TextContainer>
@@ -59,7 +67,6 @@ export const AboutUnguess = () => {
           height="294"
           src="https://www.youtube.com/embed/bPr1XILdV4g"
         ></iframe>
-        <img className="tryber" src={tryber} alt="tryber" />
       </VideoContainer>
     </SlideContainer>
   );
