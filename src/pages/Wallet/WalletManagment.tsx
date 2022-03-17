bcab5679ae69fcd6b4d58fe07383887d4e2835b9
iacopolea <iacopo.lea@gmail.com>
2022-03-17 15:16:47 +0100

    wip(wallet): page setup

--- src/pages/Wallet/WalletManagment.tsx
+++ src/pages/Wallet/WalletManagment.tsx
@@ -1,7 +1,3 @@
-import { Select, SortTableSelect } from "@appquality/appquality-design-system";
-import { useTranslation } from "react-i18next";
-
 export const WalletManagment = () => {
-  const { t } = useTranslation();
   return <div></div>;
 };
