52d1ad83b904440d1ff63fed07a5a1ab2da00bfb
iacopolea <iacopo.lea@gmail.com>
2022-03-17 15:16:15 +0100

    wip(wallet): page setup

--- src/pages/Wallet/WalletManagment.tsx
+++ src/pages/Wallet/WalletManagment.tsx
@@ -3,25 +3,5 @@ import { useTranslation } from "react-i18next";
 
 export const WalletManagment = () => {
   const { t } = useTranslation();
-  return (
-    <div>
-      <div className="aq-mb-3">
-        <Select
-          label={t("Campaign")}
-          name="campaign"
-          options={[]}
-          value={{ label: "", value: "" }}
-          isSearchable
-          placeholder={t("Search")}
-          isClearable={false}
-        />
-      </div>
-      <SortTableSelect
-        order="ASC"
-        orderBy="date"
-        columns={[]}
-        label={t("Order By", { context: "Sort Table Select" })}
-      />
-    </div>
-  );
+  return <div></div>;
 };
