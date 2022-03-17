ae4f12d84910c12aba5655450668474277f6bd05
alexSp84 <alex1984@hotmail.it>
2022-03-17 17:45:35 +0100

    wip(wallet help): init

--- src/pages/Wallet/WalletHelp.tsx
+++ src/pages/Wallet/WalletHelp.tsx
@@ -1,3 +1,31 @@
+import {
+  Button,
+  Card,
+  Text,
+  Title,
+} from "@appquality/appquality-design-system";
+import { useTranslation } from "react-i18next";
+
 export const WalletHelp = () => {
-  return <div></div>;
+  const { t } = useTranslation();
+
+  return (
+    <Card shadow>
+      <Title size="xs" color="info">
+        {t("Not sure how to request your booty?")}
+      </Title>
+      <Text className="aq-mt-2 aq-mb-3">
+        {t("Learn more about how we handle payment requests.")}
+      </Text>
+      <Button
+        forwardedAs="a"
+        href={`${t("/per-saperne-di-piu-pagamenti-privacy/")}`}
+        type="info"
+        size="block"
+        flat
+      >
+        {t("Learn more")}
+      </Button>
+    </Card>
+  );
 };
