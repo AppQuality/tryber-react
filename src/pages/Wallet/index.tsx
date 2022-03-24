60687f429c02f596e138ff0b9d1e024eda1bdba1
Davide Bizzi <davide.bizzi@app-quality.com>
2022-03-24 14:54:11 +0100

    fix(wallet): Get data for fiscal profile on page load

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -1,14 +1,22 @@
 import { BSCol, BSGrid, Card } from "@appquality/appquality-design-system";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
 import { OutsideContainer, PageTemplate } from "src/features/PageTemplate";
-import { WalletManagment } from "./WalletManagment";
+import { PaymentModal } from "src/pages/Wallet/paymentModal";
+import { getFiscalProfile } from "src/redux/user/actions/getFiscalProfile";
+
 import { WalletHelp } from "./WalletHelp";
+import { WalletManagment } from "./WalletManagment";
 import { WalletTable } from "./WalletTable";
-import React from "react";
-import { PaymentModal } from "src/pages/Wallet/paymentModal/index";
 
 export default function Wallet() {
   const { t } = useTranslation();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getFiscalProfile());
+  }, []);
 
   return (
     <PageTemplate title={t("Wallet")} route={"payments"} shouldBeLoggedIn>
