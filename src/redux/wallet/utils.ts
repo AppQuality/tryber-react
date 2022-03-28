3fee66b72cfdb25672c3c2c13da91e03c761463b
alexSp84 <alex1984@hotmail.it>
2022-03-28 12:43:05 +0200

    fix(wallet): date in the format dd/mm/yyyy

--- src/redux/wallet/utils.ts
+++ src/redux/wallet/utils.ts
@@ -9,8 +9,8 @@ export const getPaidDate = (requestDate?: string) => {
     const date = new Date(requestDate);
     return date.toLocaleDateString(undefined, {
       year: "numeric",
-      month: "numeric",
-      day: "numeric",
+      month: "2-digit",
+      day: "2-digit",
     });
   }
   return "-";
