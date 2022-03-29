c23eb893cca2082759d18ff869c97ff049c84baa
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-03-29 15:39:20 +0200

    update schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1943,7 +1943,7 @@ export interface operations {
         /** Max items to retrieve */
         limit?: components["parameters"]["limit"];
         /** The field for item order */
-        orderBy?: "id" | "attributionDate";
+        orderBy?: "id" | "attributionDate" | "amount";
         /** How to order values (ASC, DESC) */
         order?: components["parameters"]["order"];
       };
