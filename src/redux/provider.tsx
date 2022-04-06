55b37648f27d96fc6beabeecdc05747fd6fa40f1
iacopolea <iacopo.lea@gmail.com>
2022-04-06 17:23:40 +0200

    feat: support dev tools

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware, Store } from "redux";
+import { createStore, applyMiddleware, Store, compose } from "redux";
 import { Provider, useDispatch } from "react-redux";
 import thunk from "redux-thunk";
 
 import reducer from "./reducer";
 const middlewares = [thunk];
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store: Store<GeneralState, MenuAction> & {
   dispatch: DispatchType;
-} = createStore(reducer, applyMiddleware(...middlewares));
+} = createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export default ({ children }: any) => (
