678dfa9df73ec9df1aa8186e818639df489aecd0
Iacopo Leardini <iacopo.lea@gmail.com>
2022-05-03 10:45:52 +0200

    chore: update schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -299,6 +299,13 @@ export interface paths {
       };
     };
   };
+  "/users/me/rank": {
+    get: operations["get-users-me-rank"];
+    parameters: {};
+  };
+  "/users/me/rank/list": {
+    get: operations["get-users-me-rank-list"];
+  };
   "/custom_user_fields": {
     get: operations["get-customUserFields"];
     parameters: {};
@@ -347,6 +354,11 @@ export interface components {
     /** Replicability */
     Replicability: {
       id?: string;
+    };
+    /** MonthlyLevel */
+    MonthlyLevel: {
+      id: number;
+      name: string;
     };
     /** Task */
     Task: components["schemas"]["TaskOptional"] &
@@ -535,6 +547,13 @@ export interface components {
           /** @description A google maps place id */
           placeId: string;
         };
+    RankingItem: {
+      position: number;
+      image: string;
+      id: number;
+      name: string;
+      monthly_exp: number;
+    };
   };
   responses: {
     /** A user */
@@ -2157,6 +2176,46 @@ export interface operations {
             size: number;
             total?: number;
             start: number;
+          };
+        };
+      };
+      403: components["responses"]["NotAuthorized"];
+      404: components["responses"]["NotFound"];
+    };
+  };
+  "get-users-me-rank": {
+    parameters: {};
+    responses: {
+      /** OK */
+      200: {
+        content: {
+          "application/json": {
+            level: components["schemas"]["MonthlyLevel"];
+            previousLevel: components["schemas"]["MonthlyLevel"];
+            rank: number;
+            points: number;
+            prospect: {
+              level: components["schemas"]["MonthlyLevel"];
+              maintenance?: number;
+              next?: {
+                points: number;
+                level: components["schemas"]["MonthlyLevel"];
+              };
+            };
+          };
+        };
+      };
+      403: components["responses"]["NotAuthorized"];
+    };
+  };
+  "get-users-me-rank-list": {
+    responses: {
+      /** OK */
+      200: {
+        content: {
+          "application/json": {
+            tops: components["schemas"]["RankingItem"][];
+            peers: components["schemas"]["RankingItem"][];
           };
         };
       };
