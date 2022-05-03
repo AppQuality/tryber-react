692654e0fad16005d821af7810ac135295314961
Iacopo Leardini <iacopo.lea@gmail.com>
2022-05-03 10:32:02 +0200

    feat: avatar with rank

--- src/pages/Ranking/RankingRecap/AvatarWithRank.tsx
+++ src/pages/Ranking/RankingRecap/AvatarWithRank.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { rankingTheme } from "src/pages/Ranking/rankingTheme";
-import React from "react";
+import React, { useEffect } from "react";
+import getGravatarUrlWithThemedFallbackInitials from "src/utils/getGravatarUrlWithThemedFallbackInitials";
 
 interface AvatarWithRankProps {
   user: any;
@@ -8,17 +9,24 @@ interface AvatarWithRankProps {
 }
 const StyledAvatar = styled.div<{ badgeColor: string }>`
   position: relative;
+  width: 132px;
   .ranking-avatar {
-    width: 100px;
-    height: 100px;
+    width: 105px;
+    height: 105px;
     border-radius: 50%;
   }
   .badge {
     position: absolute;
-    bottom: -10px;
-    right: -10px;
+    bottom: -5px;
+    right: 0;
     color: ${(p) => p.badgeColor};
     font-size: 0.8rem;
+    width: 60px;
+    height: 60px;
+    svg {
+      width: 100%;
+      height: 100%;
+    }
   }
 `;
 
@@ -27,11 +35,16 @@ export const AvatarWithRank = ({
   rankingSummary,
 }: AvatarWithRankProps) => {
   const { id } = rankingSummary.level;
-  const badgeColor = rankingTheme[id].textColor;
-
+  const badgeColor = rankingTheme[id].main;
+  const [avatarUrl, setAvatarUrl] = React.useState<string>("");
+  useEffect(() => {
+    setAvatarUrl(
+      getGravatarUrlWithThemedFallbackInitials(user.image, badgeColor)
+    );
+  }, [user.image, badgeColor]);
   return (
     <StyledAvatar badgeColor={badgeColor}>
-      <img className="ranking-avatar" src={user?.image} alt={user?.name} />
+      <img className="ranking-avatar" src={avatarUrl} alt={user?.name} />
       <span className="badge">{rankingTheme[id].icon}</span>
     </StyledAvatar>
   );
