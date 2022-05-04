b4acd64f3d6fe02b72c6c637a43a7d5e2eb44867
alexSp84 <alessandro.spinelli84@outlook.it>
2022-05-04 10:41:44 +0200

    chore(ranking): refactor

--- src/utils/ranking/index.ts
+++ src/utils/ranking/index.ts
@@ -1,13 +1,2 @@
-import apifetch from "src/utils/apifetch";
-
-export const getRankings = async (
-  token?: string
-): Promise<
-  ApiOperations["get-users-me-rank-list"]["responses"]["200"]["content"]["application/json"]
-> => {
-  return apifetch({
-    endpoint: "/users/me/rank/list",
-    token: token,
-  });
-};
 export * from "./getUserRankingSummary";
+export * from "./getUserRankings";
