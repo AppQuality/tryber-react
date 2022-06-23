df3edf06bda8c23e0f11e29c93794fa5ad61fb4a
alexSp84 <alessandro.spinelli84@outlook.it>
2022-06-23 17:34:41 +0200

    fix(bug form)

--- src/pages/BugForm/index.tsx
+++ src/pages/BugForm/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { DatepickerGlobalStyle } from "@appquality/appquality-design-system";
 import { PageTemplate } from "../../features/PageTemplate";
 import { BugFormContainer } from "./BugFormContainer/BugFormContainer";
-import { BugFormNoDevice } from "./BugFormNoDevice/BugFormNoDevice";
 
 export default function BugForm() {
   return (
     <PageTemplate title={"Bug form"} route={"bug-form"} shouldBeLoggedIn>
       <DatepickerGlobalStyle />
-      <BugFormNoDevice />
+      <BugFormContainer />
     </PageTemplate>
   );
 }
