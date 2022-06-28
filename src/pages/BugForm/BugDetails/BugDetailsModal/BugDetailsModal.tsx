17e6974e9b8261dcc4bc3670d83cb896108e0c1a
alexSp84 <alessandro.spinelli84@outlook.it>
2022-06-28 10:23:38 +0200

    fix(bug form): remove default value

--- src/pages/BugForm/BugDetails/BugDetailsModal/BugDetailsModal.tsx
+++ src/pages/BugForm/BugDetails/BugDetailsModal/BugDetailsModal.tsx
@@ -23,103 +23,11 @@ export const BugDetailsModal = () => {
   const getContent = () => {
     switch (type) {
       case "severity":
-        return t("BUGFORM_SEVERITY_MODAL_TXT", {
-          defaultValue: (
-            <>
-              <strong>Low</strong>
-              <div className="aq-mb-3">
-                Issues related to unresponsive and/or crashing software. When
-                the user needs to reset or relaunch it to keep using it (i.e.
-                apps closing unexpectedly, neverending loadings, 404 error
-                pages,...).
-              </div>
-              <strong>Medium</strong>
-              <div className="aq-mb-3">
-                Issues related to the visual aspect of the product, such as
-                overlapping elements, wrong contrast, incoherent elements, cut
-                or blurry text or media, elements that are either out of the
-                screen or partially not visible/overlapped.
-              </div>
-              <strong>Hight</strong>
-              <div className="aq-mb-3">
-                Issues strictly related to the contents load speed or the
-                completion time processes/actions (lag, slowing down, delays).
-                In the case of a never-ending loading, the correct bug type is
-                CRASH.
-              </div>
-              <strong>Critical</strong>
-              <div>
-                Issues related to a wrong functioning or non functioning feature
-                of the product. When the feature works, but the result is
-                different than what the user would expect, the bug type is
-                USABILITY.
-              </div>
-            </>
-          ),
-        });
+        return t("BUGFORM_SEVERITY_MODAL_TXT");
       case "type":
-        return t("BUGFORM_TYPE_MODAL_TEXT", {
-          defaultValue: (
-            <>
-              <strong>Crash</strong>
-              <div className="aq-mb-3">
-                Issues related to unresponsive and/or crashing software. When
-                the user needs to reset or relaunch it to keep using it (i.e.
-                apps closing unexpectedly, neverending loadings, 404 error
-                pages,...).
-              </div>
-              <strong>Graphic</strong>
-              <div className="aq-mb-3">
-                Issues related to the visual aspect of the product, such as
-                overlapping elements, wrong contrast, incoherent elements, cut
-                or blurry text or media, elements that are either out of the
-                screen or partially not visible/overlapped.
-              </div>
-              <strong>Performance</strong>
-              <div className="aq-mb-3">
-                Issues strictly related to the contents load speed or the
-                completion time processes/actions (lag, slowing down, delays).
-                In the case of a never-ending loading, the correct bug type is
-                CRASH.
-              </div>
-              <strong>Malfunction</strong>
-              <div>
-                Issues related to a wrong functioning or non functioning feature
-                of the product. When the feature works, but the result is
-                different than what the user would expect, the bug type is
-                USABILITY.
-              </div>
-            </>
-          ),
-        });
+        return t("BUGFORM_TYPE_MODAL_TEXT");
       case "replicability":
-        return t("BUGFORM_REPLICABILTY_MODAL_TXT", {
-          defaultValue: (
-            <>
-              <strong>Sometimes</strong>
-              <div className="aq-mb-3">
-                Issues related to unresponsive and/or crashing software. When
-                the user needs to reset or relaunch it to keep using it (i.e.
-                apps closing unexpectedly, neverending loadings, 404 error
-                pages,...).
-              </div>
-              <strong>Always</strong>
-              <div className="aq-mb-3">
-                Issues related to the visual aspect of the product, such as
-                overlapping elements, wrong contrast, incoherent elements, cut
-                or blurry text or media, elements that are either out of the
-                screen or partially not visible/overlapped.
-              </div>
-              <strong>Once</strong>
-              <div className="aq-mb-3">
-                Issues strictly related to the contents load speed or the
-                completion time processes/actions (lag, slowing down, delays).
-                In the case of a never-ending loading, the correct bug type is
-                CRASH.
-              </div>
-            </>
-          ),
-        });
+        return t("BUGFORM_REPLICABILTY_MODAL_TXT");
       default:
         return <></>;
     }
