7ecb7bbaca3cf979f15984b323f5b2bc909571cc
Davide Bizzi <davide.bizzi@app-quality.com>
2022-07-01 11:47:32 +0200

    fix: Fix bugform routing

--- src/pages/BugForm/index.tsx
+++ src/pages/BugForm/index.tsx
@@ -4,32 +4,42 @@ import { OutsideContainer, PageTemplate } from "src/features/PageTemplate";
 import { BugFormContainer } from "src/pages/BugForm/BugFormContainer";
 import { BugDetailsModal } from "src/pages/BugForm/BugDetails/BugDetailsModal/BugDetailsModal";
 import { BugFormUnauthorized } from "./BugFormErrorPages/BugFormUnauthorized";
+import { BugFormNoDevice } from "./BugFormErrorPages/BugFormNoDevice";
 import useCampaignData from "./useCampaignData";
 import Loading from "src/features/Loading";
 import { useTranslation } from "react-i18next";
 
 export default function BugForm() {
-  const { data, isError, isFetching } = useCampaignData();
+  const { data, isError, noDevice, isFetching, campaignId } = useCampaignData();
   const { t } = useTranslation();
+  const route = `campaign/${campaignId}/bugform`;
 
   if (isFetching) {
     return (
-      <PageTemplate route={"bug-form"} shouldBeLoggedIn>
+      <PageTemplate route={route} shouldBeLoggedIn>
         <Loading />
       </PageTemplate>
     );
   }
   if (isError || !data?.hasBugForm) {
     return (
-      <PageTemplate route={"bug-form"} shouldBeLoggedIn>
+      <PageTemplate route={route} shouldBeLoggedIn>
         <BugFormUnauthorized />
+      </PageTemplate>
+    );
+  }
+
+  if (noDevice) {
+    return (
+      <PageTemplate route={route} shouldBeLoggedIn>
+        <BugFormNoDevice />
       </PageTemplate>
     );
   }
   return (
     <PageTemplate
       title={t("BUGFORM_MAINTITLE", { defaultValue: "Bug form" })}
-      route={"bug-form"}
+      route={route}
       shouldBeLoggedIn
     >
       <DatepickerGlobalStyle />
