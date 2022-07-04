0f1962c0e155bcf671e541ce6d55da34f358af84
Iacopo Leardini <iacopo.lea@gmail.com>
2022-07-04 13:30:07 +0200

    fix(bugform): additional fields regex and error messages

--- src/pages/BugForm/AdditionalFields/TextAdditionalField.tsx
+++ src/pages/BugForm/AdditionalFields/TextAdditionalField.tsx
@@ -15,21 +15,23 @@ export const TextAdditionalField = ({
 }: {
   label: string;
   name: string;
-  validation: string;
-  errorMessage: string;
+  validation?: string;
+  errorMessage?: string;
 }) => {
   const { t } = useTranslation();
   const validate = (value: string) => {
     if (!value) {
-      return t("This is a required field");
+      return errorMessage ? errorMessage : t("This is a required field");
     }
-    if (!new RegExp(validation).test(value)) {
-      return errorMessage ? errorMessage : t("This is an invalid format");
+    if (validation && !new RegExp(validation).test(value)) {
+      return errorMessage
+        ? errorMessage
+        : t("Additional field default message:::This is an invalid format");
     }
   };
 
   return (
-    <Field name={name} validate={validate}>
+    <Field name={name} validate={validation ? validate : undefined}>
       {({ field, meta }: FieldProps) => {
         return (
           <FormGroup className="aq-mb-3">
