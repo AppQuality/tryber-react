b23db39b943195a7777f582913b41970f581e392
alexSp84 <alessandro.spinelli84@outlook.it>
2022-10-03 15:11:38 +0200

    feat(preview selection form): inserted the form into the page template

--- src/pages/PreviewSelectionForm/index.tsx
+++ src/pages/PreviewSelectionForm/index.tsx
@@ -1,18 +1,22 @@
 import { BSCol, BSGrid } from "@appquality/appquality-design-system";
+import { PageTemplate } from "src/features/PageTemplate";
 import { SelectionForm } from "./SelectionForm/SelectionForm";
-import { SelectionFormHeader } from "./SelectionFormHeader";
 
 export default function PreviewSelectionForm() {
   return (
-    <BSGrid>
-      <BSCol size="col-lg-9" className="aq-mb-3">
-        <SelectionFormHeader
-          title="È il tuo momento, candidati ora"
-          subtitle="Tra te e la campagna c’è questo form: compilalo in tutti i suoi campi per avere maggiori possibilità di selezione. Salveremo questi dati per campagne future."
-        />
-        <SelectionForm />
-      </BSCol>
-      <BSCol size="col-lg-3"></BSCol>
-    </BSGrid>
+    <PageTemplate
+      route="preview-selection-form"
+      title="È il tuo momento, candidati ora"
+      subtitle="Tra te e la campagna c’è questo form: compilalo in tutti i suoi campi per avere maggiori possibilità di selezione. Salveremo questi dati per campagne future."
+      shouldBeLoggedIn
+      showHeader={false}
+      showSidebar={false}
+    >
+      <BSGrid>
+        <BSCol size="col-lg-9" className="aq-mb-3">
+          <SelectionForm />
+        </BSCol>
+      </BSGrid>
+    </PageTemplate>
   );
 }
