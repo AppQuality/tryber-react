51a8aafab9f2d6a3824f7d50fecc1fcfc4b2ae31
alexSp84 <alessandro.spinelli84@outlook.it>
2022-10-03 15:39:55 +0200

    feat(preview selection form): page template style

--- src/pages/PreviewSelectionForm/index.tsx
+++ src/pages/PreviewSelectionForm/index.tsx
@@ -1,22 +1,34 @@
-import { BSCol, BSGrid } from "@appquality/appquality-design-system";
+import {
+  aqBootstrapTheme,
+  BSCol,
+  BSGrid,
+} from "@appquality/appquality-design-system";
 import { PageTemplate } from "src/features/PageTemplate";
+import styled from "styled-components";
 import { SelectionForm } from "./SelectionForm/SelectionForm";
+
+const StyledPageTemplate = styled.div`
+  background-color: ${aqBootstrapTheme.colors.purple100};
+`;
 
 export default function PreviewSelectionForm() {
   return (
-    <PageTemplate
-      route="preview-selection-form"
-      title="È il tuo momento, candidati ora"
-      subtitle="Tra te e la campagna c’è questo form: compilalo in tutti i suoi campi per avere maggiori possibilità di selezione. Salveremo questi dati per campagne future."
-      shouldBeLoggedIn
-      showHeader={false}
-      showSidebar={false}
-    >
-      <BSGrid>
-        <BSCol size="col-lg-9" className="aq-mb-3">
-          <SelectionForm />
-        </BSCol>
-      </BSGrid>
-    </PageTemplate>
+    <StyledPageTemplate>
+      <PageTemplate
+        route="preview-selection-form"
+        title="È il tuo momento, candidati ora"
+        subtitle="Tra te e la campagna c’è questo form: compilalo in tutti i suoi campi per avere maggiori possibilità di selezione. Salveremo questi dati per campagne future."
+        containerClass="aq-pt-1"
+        shouldBeLoggedIn
+        showHeader={false}
+        showSidebar={false}
+      >
+        <BSGrid>
+          <BSCol size="col-lg-9" className="aq-mb-3">
+            <SelectionForm />
+          </BSCol>
+        </BSGrid>
+      </PageTemplate>
+    </StyledPageTemplate>
   );
 }
