5727570ba93e75622f3fc606e9112b0671075707
alexSp84 <alessandro.spinelli84@outlook.it>
2022-10-04 09:41:22 +0200

    feat(preview selection form): style

--- src/pages/PreviewSelectionForm/index.tsx
+++ src/pages/PreviewSelectionForm/index.tsx
@@ -9,6 +9,10 @@ import { SelectionForm } from "./SelectionForm/SelectionForm";
 
 const StyledPageTemplate = styled.div`
   background-color: ${aqBootstrapTheme.colors.purple100};
+  .preview-selection-form {
+    max-width: 100%;
+    padding: 0 16px;
+  }
 `;
 
 export default function PreviewSelectionForm() {
@@ -18,7 +22,7 @@ export default function PreviewSelectionForm() {
         route="preview-selection-form"
         title="È il tuo momento, candidati ora"
         subtitle="Tra te e la campagna c’è questo form: compilalo in tutti i suoi campi per avere maggiori possibilità di selezione. Salveremo questi dati per campagne future."
-        containerClass="aq-pt-1"
+        containerClass="aq-pt-1 preview-selection-form"
         shouldBeLoggedIn
         showHeader={false}
         showSidebar={false}
