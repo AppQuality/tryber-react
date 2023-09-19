f2ec265a704ec43f2408c3218eccaaa10592ac07
Marco Bonomo <marco.bonomo@unguess.io>
2023-09-19 13:06:23 +0200

    🔥 refactor(e2e tests): remove unnecessary intercepts in wallet tests

--- cypress/e2e/wallet/table_net_gross.spec.cy.ts
+++ cypress/e2e/wallet/table_net_gross.spec.cy.ts
@@ -1,49 +1,5 @@
 describe("Net and gross columns in wallet table", () => {
   beforeEach(() => {
-    cy.intercept(
-      "GET",
-      `${Cypress.env(
-        "REACT_APP_API_URL"
-      )}/users/me?fields=pending_booty%2Cbooty_threshold`,
-      {
-        fixture: "/users/me/_get/200_pending_booty_example",
-        statusCode: 200,
-      }
-    ).as("getUserBooty");
-
-    cy.intercept(
-      "GET",
-      `${Cypress.env(
-        "REACT_APP_API_URL"
-      )}/users/me/payments?order=DESC&orderBy=paidDate&limit=10&start=0`,
-      {
-        fixture: "/users/me/payments/_get/200_multiple-pages",
-        statusCode: 200,
-      }
-    ).as("getUserPayments");
-
-    cy.intercept(
-      "GET",
-      `${Cypress.env(
-        "REACT_APP_API_URL"
-      )}/users/me/payments?order=DESC&orderBy=paidDate&limit=1&start=0`,
-      {
-        fixture: "/users/me/payments/_get/200_single-paid-payment",
-        statusCode: 200,
-      }
-    ).as("getUserPaymentPaid");
-
-    cy.intercept(
-      "GET",
-      `${Cypress.env(
-        "REACT_APP_API_URL"
-      )}/users/me/payments/2?order=DESC&orderBy=date&limit=10&start=0`,
-      {
-        fixture: "/users/me/payments/_get/200_single-paid-payment",
-        statusCode: 200,
-      }
-    ).as("getUserPaymentPendingSingle");
-
     cy.visit("/payments");
   });
 
