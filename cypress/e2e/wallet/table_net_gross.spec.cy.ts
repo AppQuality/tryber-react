ea70e548ef2102dd9f7ee78e68adc2b083d96d6a
Marco Bonomo <marco.bonomo@unguess.io>
2023-09-20 11:28:20 +0200

    🔧 fix(table_net_gross.spec.cy.ts): add intercepts for API requests in beforeEach block ✨ feat(table_net_gross.spec.cy.ts): add test to check net and gross values in wallet table

--- cypress/e2e/wallet/table_net_gross.spec.cy.ts
+++ cypress/e2e/wallet/table_net_gross.spec.cy.ts
@@ -1,5 +1,21 @@
 describe("Net and gross columns in wallet table", () => {
   beforeEach(() => {
+    cy.intercept(
+      "GET",
+      `${Cypress.env(
+        "REACT_APP_API_URL"
+      )}/users/me?fields=name%2Csurname%2Cimage%2Conboarding_completed%2Cemail%2Cwp_user_id`,
+      {
+        statusCode: 200,
+        fixture: "users/me/_get/200_Example_1.json",
+      }
+    );
+
+    cy.intercept("GET", `${Cypress.env("REACT_APP_API_URL")}/users/me/fiscal`, {
+      statusCode: 200,
+      fixture: "users/me/fiscal/_get/200_non_italian",
+    });
+
     cy.visit("/payments");
   });
 
@@ -11,9 +27,11 @@ describe("Net and gross columns in wallet table", () => {
     });
   });
 
-  it('Should show a dash ("-") in the net column and the gross in the gross column if the user has requested a payment without a net', () => {
+  it.only("Should show net and gross values in wallet table", () => {
     cy.get(".wallet-table").within(() => {
-      cy.get(".tbody:nth-child(n + 3)").contains("-");
+      cy.get(".tbody").should("exist");
+      cy.get(".tbody:nth-child(n+2)").should("not.be.empty");
+      cy.get(".tbody:nth-child(n+3)").should("not.be.empty");
     });
   });
 });
