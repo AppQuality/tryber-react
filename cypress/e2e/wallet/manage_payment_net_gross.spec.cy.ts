36d2166b5accbec5324dd54f99e33b1c885ac0c8
Iacopo Leardini <iacopo.lea@gmail.com>
2023-09-20 18:43:45 +0200

    test(net-gross): setup tests

--- cypress/e2e/wallet/manage_payment_net_gross.spec.cy.ts
+++ cypress/e2e/wallet/manage_payment_net_gross.spec.cy.ts
@@ -1,5 +1,13 @@
-describe("Net and gross columns in manage your payment card", () => {
+describe("The payment manager card", () => {
   beforeEach(() => {
     cy.visit("/payments");
   });
+  it("should display both net and gross values if api response has net value", () => {});
+});
+
+describe("The request payment modal", () => {
+  beforeEach(() => {
+    cy.visit("/payments");
+  });
+  it("should display both net and gross values if api response has net value", () => {});
 });
