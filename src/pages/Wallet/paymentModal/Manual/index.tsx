95f693a5b497660822d816192bfff9d003d12f97
Iacopo Leardini <iacopo.lea@gmail.com>
2023-10-18 20:12:52 +0200

    feat(manual-payment): form first step
    Co-authored-by: d-beezee <d-beezee@users.noreply.github.com>

--- src/pages/Wallet/paymentModal/Manual/index.tsx
+++ src/pages/Wallet/paymentModal/Manual/index.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
 import { shallowEqual, useSelector } from "react-redux";
-import { Formik, Modal, ModalBody } from "@appquality/appquality-design-system";
+import {
+  Formik,
+  Modal,
+  ModalBody,
+  Text,
+  Field,
+  FormGroup,
+  Checkbox,
+  FormikField,
+} from "@appquality/appquality-design-system";
 import { setPaymentModalOpen } from "src/redux/wallet/actionCreator";
 import { useAppDispatch } from "src/redux/provider";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
+import * as yup from "yup";
+import { ErrorMessage, FieldProps, FormikHelpers } from "formik";
+import { BaseSyntheticEvent } from "react";
+import { Footer } from "./Footer";
 
 const Manual = () => {
   const dispatch = useAppDispatch();
@@ -13,8 +25,50 @@ const Manual = () => {
     (state: GeneralState) => state.wallet,
     shallowEqual
   );
+  type PaymentFormType = {
+    termsAcceptance: boolean;
+    bankaccountOwner: string;
+    iban: string;
+  };
+  const initialValues: PaymentFormType = {
+    termsAcceptance: false,
+    bankaccountOwner: "",
+    iban: "",
+  };
+  const validationSchema = yup.object({
+    termsAcceptance: yup
+      .boolean()
+      .required(t("This is a required field"))
+      .oneOf([true], t("you must accept terms and conditions")),
+    bankaccountOwner: yup
+      .string()
+      .required(t("This is a required field"))
+      .matches(/^.+ .+$/gi, t("Insert Name and surname separated by space"))
+      .matches(
+        /^[A-Za-zÀ-ÖØ-öø-ÿ'-ū.]+ ['A-Za-zÀ-Ö Ø-öø-ÿ-ū.]+$/gi,
+        t("The account holder name should contain latin character only")
+      ),
+    iban: yup
+      .string()
+      .required("This is a required field")
+      .matches(
+        /^([A-Z]{2}[ \-]?[0-9]{2})(?=(?:[ \-]?[A-Z0-9]){9,30}$)((?:[ \-]?[A-Z0-9]{3,5}){2,7})([ \-]?[A-Z0-9]{1,3})?$/g,
+        t("This is an invalid format.")
+      ),
+  });
+  const onSubmit = async (
+    values: PaymentFormType,
+    formikHelper: FormikHelpers<PaymentFormType>
+  ) => {
+    formikHelper.setSubmitting(true);
+    formikHelper.setSubmitting(false);
+  };
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {(formikProps) => {
         const closeModal = () => {
           formikProps.resetForm();
@@ -29,10 +83,69 @@ const Manual = () => {
               isOpen={isPaymentModalOpen}
               onClose={closeModal}
               title={t("Request a payment")}
-              footer={"footer"}
+              footer={<Footer />}
             >
               <ModalBody>
-                <div data-qa="manual-payment-modal">manual</div>
+                <div data-qa="manual-payment-modal">
+                  <Text data-qa="manual-payment-modal-introduction-text">
+                    {t("__MODAL_MANUAL_PAYMENT_INTRODUCTION_TEXT__")}
+                  </Text>
+                  <Field
+                    name="bankaccountOwner"
+                    type="text"
+                    label={t("Account Holder")}
+                    placeholder={t("name")}
+                    data-qa="manual-payment-modal-account-holder"
+                  />
+                  <Field
+                    name="iban"
+                    type="text"
+                    label={t("IBAN")}
+                    placeholder="OT2T26635625325382772"
+                    data-qa="manual-payment-modal-iban"
+                  />
+                  <FormikField name="termsAcceptance" className="aq-mb-3">
+                    {({ field, form, meta }: FieldProps) => {
+                      const onCheckChange = (e: BaseSyntheticEvent) => {
+                        field.onChange(e);
+                        form.setFieldValue(field.name, e.target.checked);
+                      };
+                      return (
+                        <FormGroup data-qa="manual-payment-modal-terms">
+                          <Checkbox
+                            id="termsAcceptance"
+                            name={field.name}
+                            onChange={onCheckChange}
+                            checked={field.value}
+                            label={
+                              <strong className="aq-text-primary">
+                                <Trans
+                                  i18nKey={
+                                    "Accept the <terms_and_conditions_link>conditions</terms_and_conditions_link> before proceeding"
+                                  }
+                                  components={{
+                                    terms_and_conditions_link: (
+                                      <a
+                                        data-qa="manual-payment-modal-terms-and-conditions-link"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        href={t("/payment_conditions", {
+                                          ns: "links",
+                                        })}
+                                      />
+                                    ),
+                                  }}
+                                />
+                              </strong>
+                            }
+                            onBlur={field.onBlur}
+                          />
+                          <ErrorMessage name={field.name} />
+                        </FormGroup>
+                      );
+                    }}
+                  </FormikField>
+                </div>
               </ModalBody>
             </Modal>
           </form>
