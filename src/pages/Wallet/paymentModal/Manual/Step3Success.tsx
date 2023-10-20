c73bb806ccee4fbf03b39cbd0fafded24937dc97
Iacopo Leardini <iacopo.lea@gmail.com>
2023-10-20 15:36:20 +0200

    feat(payments): step 3 text

--- src/pages/Wallet/paymentModal/Manual/Step3Success.tsx
+++ src/pages/Wallet/paymentModal/Manual/Step3Success.tsx
@@ -1,14 +1,45 @@
-import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
+import { Check2Circle } from "react-bootstrap-icons";
+import styled from "styled-components";
+import { aqBootstrapTheme, Text } from "@appquality/appquality-design-system";
 
 const SuccessMessage = () => {
-  return <div data-qa="manual-payment-modal-success-text">Success</div>;
+  return (
+    <div data-qa="manual-payment-modal-success-text">
+      <Text className="aq-text-center">
+        <Trans
+          i18nKey="available tags: <br>, <bold>:::PAYMENTS_MODAL_INVOICE_STEP_4_SUCCESS"
+          components={{
+            br: <br />,
+            bold: <strong className="aq-text-primary" />,
+            b: <strong className="aq-text-primary" />,
+          }}
+          defaults={
+            "<bold>Almost there!</bold><br />You will receive an email with the payment details. <br> Thank you for your collaboration!"
+          }
+        />
+      </Text>
+    </div>
+  );
 };
 
+const Styled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: ${(p) => p.theme.grid.sizes[4]};
+`;
+
 export const Step3Success = () => {
-  const { t } = useTranslation();
   return (
-    <div data-qa="manual-payment-modal-step-4">
+    <Styled data-qa="manual-payment-modal-step-4">
+      <Check2Circle
+        className="aq-mb-3"
+        color={aqBootstrapTheme.palette.success}
+        size={32}
+      />
       <SuccessMessage />
-    </div>
+    </Styled>
   );
 };
