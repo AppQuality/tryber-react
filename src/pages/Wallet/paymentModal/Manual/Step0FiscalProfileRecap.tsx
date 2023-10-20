a1e9e24859f92e5b36c276f138aa15544c25ca3d
Davide Bizzi <davide.bizzi@unguess.io>
2023-10-20 15:36:21 +0200

    feat: Add fiscal type

--- src/pages/Wallet/paymentModal/Manual/Step0FiscalProfileRecap.tsx
+++ src/pages/Wallet/paymentModal/Manual/Step0FiscalProfileRecap.tsx
@@ -1,11 +1,38 @@
 import { useTranslation } from "react-i18next";
+import {
+  GetUsersMeFiscalApiResponse,
+  useGetUsersMeFiscalQuery,
+} from "src/services/tryberApi";
+
+const FiscalTypeItem = ({
+  type,
+}: {
+  type: GetUsersMeFiscalApiResponse["type"];
+}) => {
+  const { t } = useTranslation();
+
+  switch (type) {
+    case "vat":
+      return <>{t("VAT rate scheme")}</>;
+    case "witholding-extra":
+      return <>{t("Annual witholding > 5000")}</>;
+    case "company":
+      return <>{t("Company rate scheme")}</>;
+    default:
+      throw new Error("Invalid fiscal type");
+  }
+};
 
 export const Step0FiscalProfileRecap = () => {
+  const { data, isLoading } = useGetUsersMeFiscalQuery();
   const { t } = useTranslation();
+
+  if (isLoading || !data) return null;
   return (
     <div data-qa="manual-payment-fiscal-profile-recap">
       <div className="aq-mb-1">
         <strong className="aq-text-primary">{t("Fiscal profile recap")}</strong>
+        <FiscalTypeItem type={data.type} />
       </div>
     </div>
   );
