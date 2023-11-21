d1db0789ba00da24f211db4c68996a3ab198bf35
Iacopo Leardini <iacopo.lea@gmail.com>
2023-11-21 12:45:56 +0100

    wip: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    // trace: "on-first-retry",
+    trace: "on",
+    video: "on",
+    screenshot: "on",
     testIdAttribute: "data-qa",
     baseURL: "http://localhost:3000",
   },
