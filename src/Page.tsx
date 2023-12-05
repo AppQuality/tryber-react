1cd9d500bff14dc2a72693c873ad6930f8f5cc38
Iacopo Leardini <iacopo.lea@gmail.com>
2023-12-05 13:12:44 +0100

    feat(sentry): conditionalli start sentry

--- src/Page.tsx
+++ src/Page.tsx
@@ -34,29 +34,31 @@ import isStagingEnvironment from "./features/isStagingEnvironment";
 const SentryRoute = Sentry.withSentryRouting(Route);
 const history = createBrowserHistory();
 
-Sentry.init({
-  dsn: "https://84fe7a6107da4c0058197b52ce74743d@o1087982.ingest.sentry.io/4506337899511808",
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
-    }),
-  ],
-  environment: _env_.REACT_APP_ENVIRONMENT,
-  // trace all staging and locale traces and 70% of production traces
-  tracesSampleRate: isStagingEnvironment() ? 1.0 : 0.7,
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [
-    "localhost",
-    /^https:\/\/dev\.tryber\.me\/api/,
-    /^https:\/\/tryber\.me\/api/,
-  ],
+if (process.env.NODE_ENV !== "test") {
+  Sentry.init({
+    dsn: "https://84fe7a6107da4c0058197b52ce74743d@o1087982.ingest.sentry.io/4506337899511808",
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+      }),
+    ],
+    environment: _env_.REACT_APP_ENVIRONMENT,
+    // trace all staging and locale traces and 70% of production traces
+    tracesSampleRate: isStagingEnvironment() ? 1.0 : 0.7,
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [
+      "localhost",
+      /^https:\/\/dev\.tryber\.me\/api/,
+      /^https:\/\/tryber\.me\/api/,
+    ],
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  // do not capture for staging and locale
-  replaysSessionSampleRate: isStagingEnvironment() ? 0.0 : 0.1,
-  replaysOnErrorSampleRate: isStagingEnvironment() ? 0.0 : 1.0,
-});
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    // do not capture for staging and locale
+    replaysSessionSampleRate: isStagingEnvironment() ? 0.0 : 0.1,
+    replaysOnErrorSampleRate: isStagingEnvironment() ? 0.0 : 1.0,
+  });
+}
 
 if (process.env.REACT_APP_DATADOG_CLIENT_TOKEN) {
   datadogLogs.init({
