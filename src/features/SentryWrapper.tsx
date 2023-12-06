8dc053e282c64295a17016db507244094c1ccfaf
Iacopo Leardini <iacopo.lea@gmail.com>
2023-12-06 12:36:40 +0100

    fix(sentry): Update SentryWrapper to set user properties

--- src/features/SentryWrapper.tsx
+++ src/features/SentryWrapper.tsx
@@ -3,12 +3,16 @@ import * as Sentry from "@sentry/react";
 import i18n from "src/i18n";
 
 const SentryWrapper = ({ children }: { children: React.ReactNode }) => {
-  const { data: user } = useGetUsersMeQuery({});
+  const { data: user } = useGetUsersMeQuery({
+    fields: "id, email, username, wp_user_id, role",
+  });
 
   Sentry.setUser({
-    role: user?.role ?? "unknown",
+    id: user?.id ?? 0,
+    email: user?.email ?? "unknown",
+    username: user?.username ?? "unknown",
     wp_user_id: user?.wp_user_id ?? 0,
-    tester_id: user?.id ?? 0,
+    role: user?.role ?? "unknown",
   });
 
   Sentry.setTag("page_locale", i18n.language);
