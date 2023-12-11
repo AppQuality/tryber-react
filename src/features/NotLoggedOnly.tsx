f8b38eb8433465b21b06c71a064025d2651935dd
Davide Bizzi <davide.bizzi@unguess.io>
2023-12-11 15:20:16 +0100

    feat: Trace user on notlogged

--- src/features/NotLoggedOnly.tsx
+++ src/features/NotLoggedOnly.tsx
@@ -1,6 +1,7 @@
 import TagManager from "react-gtm-module";
 import { useHistory } from "react-router-dom";
 
+import * as Sentry from "@sentry/react";
 import { useGetUsersMeQuery } from "src/services/tryberApi";
 import { useAppSelector } from "src/store";
 import Loading from "./Loading";
@@ -13,7 +14,13 @@ const NotLoggedOnly = ({
   children: React.ReactNode;
   redirect?: { url: string; message?: string };
 }) => {
-  const { data: user, error, isLoading } = useGetUsersMeQuery({});
+  const {
+    data: user,
+    error,
+    isLoading,
+  } = useGetUsersMeQuery({
+    fields: "id,email,username,wp_user_id,role",
+  });
   const history = useHistory();
   const isPublicUser = useAppSelector(
     (state) => state.publicUserPages.isPublic
@@ -23,6 +30,13 @@ const NotLoggedOnly = ({
     return <Loading />;
   }
   if (user && redirect) {
+    Sentry.setUser({
+      id: user?.id ?? 0,
+      email: user?.email ?? "unknown",
+      username: user?.username ?? "unknown",
+      wp_user_id: user?.wp_user_id ?? 0,
+      role: user?.role ?? "unknown",
+    });
     history.push(redirect.url);
   }
   TagManager.dataLayer({
