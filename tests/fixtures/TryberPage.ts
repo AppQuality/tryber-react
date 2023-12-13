74dd2e092adf61ce86bb329be176e7c65f59292e
Iacopo Leardini <iacopo.lea@gmail.com>
2023-12-13 18:06:42 +0100

    feat(test): update loggedin e loggedout method

--- tests/fixtures/TryberPage.ts
+++ tests/fixtures/TryberPage.ts
@@ -16,6 +16,14 @@ export class TryberPage {
         });
       }
     );
+    await this.page.route(
+      "*/**/api/users/me?fields=id%2Cemail%2Cusername%2Cwp_user_id%2Crole",
+      async (route) => {
+        await route.fulfill({
+          path: "tests/api/users/me/_get/200_Example_1.json",
+        });
+      }
+    );
     await this.page.route("*/**/api/users/me?", async (route) => {
       await route.fulfill({
         path: "tests/api/users/me/_get/200_Example_1.json",
@@ -26,6 +34,15 @@ export class TryberPage {
   async loggedOut() {
     await this.page.route(
       "*/**/api/users/me?fields=name%2Csurname%2Cimage%2Conboarding_completed%2Cemail%2Cwp_user_id",
+      async (route) => {
+        await route.fulfill({
+          status: 403,
+          json: { err: "unauthorized" },
+        });
+      }
+    );
+    await this.page.route(
+      "*/**/api/users/me?fields=id%2Cemail%2Cusername%2Cwp_user_id%2Crole",
       async (route) => {
         await route.fulfill({
           status: 403,
