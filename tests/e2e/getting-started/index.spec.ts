f82cc5859b63b8bfec30c5e832985b61a8e4699b
Iacopo Leardini <iacopo.lea@gmail.com>
2023-12-13 17:17:11 +0100

    tests(): setup for getting started pages

---
+++ tests/e2e/getting-started/index.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+import { GettingStarted } from "../../fixtures/GettingStarted";
+import { describe } from "node:test";
+
+let gettingStarted: GettingStarted;
+
+describe("if the user is logged in", () => {
+  test("the url /getting-started should redirect to the dashboard", async ({
+    page,
+  }) => {});
+  test("the url /getting-started/mail-signup should redirect to the dashboard", async ({
+    page,
+  }) => {});
+  test("the url /getting-started/confirmation should redirect to the dashboard", async ({
+    page,
+  }) => {});
+});
+
+describe("if the user is logged out", () => {
+  test("/getting-started should show the signup choice page", async ({
+    page,
+  }) => {});
+  test("/getting-started/mail-signup should show the mail signup form", async ({
+    page,
+  }) => {});
+  test("/getting-started/confirmation should redirect to /getting-started if the user doesn't come from a succesfull signup", async ({
+    page,
+  }) => {});
+  test("/getting-started/confirmation should show a confirmation message if the user succesfully signed up", async ({
+    page,
+  }) => {
+    // can't develop this test because the signup process is not implemented yet
+  });
+});
