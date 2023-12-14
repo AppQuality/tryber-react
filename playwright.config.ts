bcd51fc5c83cc58a86dc68846c5a22b7f52ea1a1
Iacopo Leardini <iacopo.lea@gmail.com>
2023-12-14 13:23:28 +0100

    restore CI webServer command in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "export NODE_ENV=test && yarn start",
+    command: process.env.CI
+      ? "export NODE_OPTIONS=--openssl-legacy-provider && export NODE_ENV=test && yarn start"
+      : "export NODE_ENV=test && yarn start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: false,
   },
