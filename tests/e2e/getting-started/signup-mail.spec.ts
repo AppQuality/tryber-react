efa383af2091f016b794935d9a8e02be4b679869
Davide Bizzi <davide.bizzi@unguess.io>
2023-12-21 14:57:57 +0100

    test: Set missing test as not implemented

--- tests/e2e/getting-started/signup-mail.spec.ts
+++ tests/e2e/getting-started/signup-mail.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { GettingStarted } from "../../fixtures/GettingStarted";
 
 let gettingStarted: GettingStarted;
@@ -29,41 +29,97 @@ test.describe("The signup mail page", () => {
     await expect(page.getByTestId("signup-stepper")).toBeVisible();
   });
   // todo: test stepper component
-  test("should display a mail field", async ({}) => {});
-  test("should display a password field", async ({}) => {});
-  test("should display a list of password requirements", async ({}) => {});
-  test("should display a submit button", async ({}) => {});
-  test("if the user click the submit button and the mail is empty should display an error message", async ({}) => {});
-  test("if the user click the submit button and the mail is not valid should display an error message", async ({}) => {});
-  test("if the user click the submit button and the password is empty should display an error message", async ({}) => {});
-  test("if the user click the submit button and the password is not valid should display an error message", async ({}) => {});
-  test("if the user click the submit but mail is already present in db an error notification should appear", async ({}) => {});
-  test("if the user click the submit button and mail and password are valid should display the second step (profile data)", async ({}) => {});
+  test("should display a mail field", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a password field", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a list of password requirements", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a submit button", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the submit button and the mail is empty should display an error message", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the submit button and the mail is not valid should display an error message", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the submit button and the password is empty should display an error message", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the submit button and the password is not valid should display an error message", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the submit but mail is already present in db an error notification should appear", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the submit button and mail and password are valid should display the second step (profile data)", async ({}) => {
+    throw new Error("Not implemented");
+  });
 });
 
 test.describe("The signup mail page second step", () => {
-  test("should display a stepper component at the second step", async ({}) => {});
-  test("should display a required name field", async ({}) => {});
-  test("should display a required last name field", async ({}) => {});
-  test("should display a required birthdate field", async ({}) => {});
-  test("should display a required country field", async ({}) => {});
-  test("should display a link privacy policy", async ({}) => {});
-  test("should display a link to ethical code", async ({}) => {});
-  test("should display a button to go back to the first step", async ({}) => {});
-  test("should display a button to submit", async ({}) => {});
+  test("should display a stepper component at the second step", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a required name field", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a required last name field", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a required birthdate field", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a required country field", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a link privacy policy", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a link to ethical code", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a button to go back to the first step", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("should display a button to submit", async ({}) => {
+    throw new Error("Not implemented");
+  });
 
-  test("if the user click the terms and conditions link another tab is opened to /terms-and-conditions in the current language", async ({}) => {});
-  test("if the user click the privacy policy link another tab is opened to https://www.iubenda.com/privacy-policy/7934311", async ({}) => {});
-  test("if the user click the ethical code link another tab is opened to /ethical-code in the current language", async ({}) => {});
+  test("if the user click the terms and conditions link another tab is opened to /terms-and-conditions in the current language", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the privacy policy link another tab is opened to https://www.iubenda.com/privacy-policy/7934311", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click the ethical code link another tab is opened to /ethical-code in the current language", async ({}) => {
+    throw new Error("Not implemented");
+  });
 
-  test("if the user click to the back button got to the first step (precompiled)", async ({}) => {});
-  test("if the user change mail or password in the first step and proceed the data are precompiled", async ({}) => {});
+  test("if the user click to the back button got to the first step (precompiled)", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user change mail or password in the first step and proceed the data are precompiled", async ({}) => {
+    throw new Error("Not implemented");
+  });
 
-  test("on focus out of the date field if the format is not gg/mm/aaaa an error is shown", async ({}) => {});
-  test("if the user click to submit and all required fields are correctly filled goes to the confirmation step", async ({}) => {});
-  test("if the user click to submit and api answer with a generic error should display a generic error notification to retry", async ({}) => {});
-  // todo test("if the user click to submit and api answer with a validation error should display a specific error message to change the field", async ({}) => {});
-  test("if the user click to submit and api answer that mail is already in db we show a specific error notification to use a different mail", async ({}) => {});
+  test("on focus out of the date field if the format is not gg/mm/aaaa an error is shown", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click to submit and all required fields are correctly filled goes to the confirmation step", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  test("if the user click to submit and api answer with a generic error should display a generic error notification to retry", async ({}) => {
+    throw new Error("Not implemented");
+  });
+  // todo test("if the user click to submit and api answer with a validation error should display a specific error message to change the field", async ({}) => {throw new Error("Not implemented")});
+  test("if the user click to submit and api answer that mail is already in db we show a specific error notification to use a different mail", async ({}) => {
+    throw new Error("Not implemented");
+  });
 });
 
 // todo: instant feedback for password requirements
