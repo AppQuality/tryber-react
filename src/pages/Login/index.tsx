8f544bb43e3a033ec75ddb30668c6c06e66e0050
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2024-01-02 14:55:15 +0100

    feat(login-page): change language with redirectTo param

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,12 +6,14 @@ import { PageTemplate } from "src/features/PageTemplate";
 
 export const Login = () => {
   const { t } = useTranslation();
+  const url = new URL(window.location.href);
+  const queryParams = new URLSearchParams(url.search);
   return (
     <PageTemplate route={"login"} shouldBeLoggedIn={false}>
       <LangMenu
-        itLink="/it/login"
-        enLink="/en/login"
-        esLink="/es/login"
+        itLink={`/it/login${getValidParams(queryParams)}`}
+        enLink={`/en/login${getValidParams(queryParams)}`}
+        esLink={`/es/login${getValidParams(queryParams)}`}
         className="aq-my-3 lang-navigation"
       />
       <Title size="l" as={"h1"} className="aq-mb-3 aq-text-center">
@@ -21,5 +23,34 @@ export const Login = () => {
     </PageTemplate>
   );
 };
+
+function getValidParams(queryParams: URLSearchParams) {
+  if (queryParams.has("redirectTo")) {
+    if (redirectIsValid(queryParams)) return `?${queryParams.toString()}`;
+    else {
+      queryParams.delete("redirectTo");
+      return `?${queryParams.toString()}`;
+    }
+  }
+  if (queryParams.size) return `?${queryParams.toString()}`;
+  return "";
+}
+
+function redirectIsValid(params: URLSearchParams) {
+  const urlRegex =
+    /^(https?:\/\/)?([\w-]+\.)*([\w-]+\.[a-z]{2,})(:\d{2,5})?(\/[^\s]*)?(\?.*)?$/;
+  const redirect = params.get("redirectTo") ?? "";
+
+  if (!redirect.length) {
+    return false;
+  }
+  if (!urlRegex.test(redirect)) {
+    return false;
+  }
+  const parsedURL = new URL(redirect);
+  return (
+    parsedURL.hostname === "dev.tryber.me" || parsedURL.hostname === "tryber.me"
+  );
+}
 
 export default Login;
