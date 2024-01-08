26c9cbac09906e4506a7945422f8373070d3b3b1
Iacopo Leardini <iacopo.lea@gmail.com>
2024-01-08 16:49:13 +0100

    Refactor SignupSuccess component layout and styles

--- src/pages/SignupSuccess/index.tsx
+++ src/pages/SignupSuccess/index.tsx
@@ -8,10 +8,19 @@ import {
   Title,
   aqBootstrapTheme,
   Text,
+  Container,
 } from "@appquality/appquality-design-system";
 import { Check2Circle } from "react-bootstrap-icons";
 import { useHistory } from "react-router-dom";
 import { useLocalizeRoute } from "src/hooks/useLocalizedRoute";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  @media (min-width: ${(p) => p.theme.grid.breakpoints.md}) {
+    max-width: 650px;
+    margin: 0 auto;
+  }
+`;
 
 export default function SignupSuccess() {
   const { t } = useTranslation();
@@ -28,43 +37,48 @@ export default function SignupSuccess() {
       showSidebar={false}
       shouldBeLoggedIn
     >
-      <LangMenu
-        itLink={`/it/getting-started/confirmation`}
-        esLink={`/es/getting-started/confirmation`}
-        enLink={`/en/getting-started/confirmation`}
-        className="aq-my-3 lang-navigation"
-      />
-      <Title size="l" as={"h1"} className="aq-mb-3 aq-text-center">
-        {t("You're a Tryber now")}
-      </Title>
-      <Card className="aq-mb-3" data-qa="signup-success">
-        <Check2Circle
-          size="32"
-          style={{ verticalAlign: "sub", orizontalAlign: "center" }}
-          color={aqBootstrapTheme.palette.success}
+      <Container>
+        <LangMenu
+          itLink={`/it/getting-started/confirmation`}
+          esLink={`/es/getting-started/confirmation`}
+          enLink={`/en/getting-started/confirmation`}
+          className="aq-my-3 lang-navigation"
         />
-        <Text className="aq-pt-3 aq-text-primary">
-          <Trans
-            i18nKey={"WELCOME_SIGNUP_MESSAGE"}
-            components={{
-              strong: <strong />,
-              br: <br />,
-            }}
-            defaults="<strong>Welcome aboard!</strong><br />
-                We have sent you a welcome email to the address you used in the sign up process. 
+        <Wrapper>
+          <Title size="l" as={"h1"} className="aq-mb-3 aq-text-center">
+            {t("You're a Tryber now")}
+          </Title>
+          <Card className="aq-mb-3 aq-text-center" data-qa="signup-success">
+            <Check2Circle
+              size="32"
+              style={{ verticalAlign: "sub", orizontalAlign: "center" }}
+              color={aqBootstrapTheme.palette.success}
+              className="aq-mb-3"
+            />
+            <Text className="aq-mb-4 aq-text-primary">
+              <Trans
+                i18nKey={"WELCOME_SIGNUP_MESSAGE"}
+                components={{
+                  strong: <strong />,
+                  br: <br />,
+                }}
+                defaults="<strong>Welcome aboard!</strong><br />
+                We have sent you a welcome email to the address you used in the sign up process.<br />
                 Now it’s time to explore the TRYBER universe: choose among our University courses, apply for test campaigns and join our community!"
-          />
-        </Text>{" "}
-        <Button
-          className="aq-mb-3"
-          style={{ gridColumn: "auto / span 3" }}
-          kind="primary"
-          size="block"
-          onClick={goToDashboard}
-        >
-          {t("Go to dashboard")}
-        </Button>
-      </Card>
+              />
+            </Text>{" "}
+            <Button
+              className="aq-mb-3"
+              style={{ gridColumn: "auto / span 3" }}
+              kind="primary"
+              size="block"
+              onClick={goToDashboard}
+            >
+              {t("Go to dashboard")}
+            </Button>
+          </Card>
+        </Wrapper>
+      </Container>
     </PageTemplate>
   );
 }
