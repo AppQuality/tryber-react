dcb82a1560dc4ac8d54b6c7b7ddf4a255311665c
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2024-01-11 10:12:59 +0100

    Fix trailing slash issue in useLocalizedRoute

--- src/hooks/useLocalizedRoute.ts
+++ src/hooks/useLocalizedRoute.ts
@@ -7,5 +7,5 @@ export function useLocalizeRoute(route: CrowdRoutes): string {
     i18n.language === "en" ? `/${route}` : `/${i18n.language}/${route}`;
   // in case of base route ("") we already have a forward slash
   let re = /\/$/;
-  return re.test(localizedRoute) ? localizedRoute : `${localizedRoute}/`;
+  return re.test(localizedRoute) ? localizedRoute : `${localizedRoute}`;
 }
