d3aec45913372bfe0f0e7e0405e436a420507928
Davide Bizzi <davide.bizzi@unguess.io>
2024-01-15 14:50:35 +0100

    rework: Use rtk on user on ranking

--- src/pages/Ranking/RankingRecap/RankingRecap.tsx
+++ src/pages/Ranking/RankingRecap/RankingRecap.tsx
@@ -1,9 +1,10 @@
 import { shallowEqual, useSelector } from "react-redux";
 import { SkeletonHeader } from "src/pages/Profile/SkeletonHeader";
 import { AvatarWithRank } from "src/pages/Ranking/RankingRecap/AvatarWithRank";
+import { useGetUsersMeQuery } from "src/services/tryberApi";
+import styled from "styled-components";
 import { CurrentSituation } from "./CurrentSituation";
 import { NextMonthSituation } from "./NextMonthSituation";
-import styled from "styled-components";
 
 const HeaderRecap = styled.div`
   .left-recap {
@@ -18,19 +19,15 @@ const HeaderRecap = styled.div`
 `;
 
 export const RankingRecap = () => {
-  const { user, loading } = useSelector(
-    (state: GeneralState) => ({
-      user: state.user.user,
-      loading: state.user.loading,
-    }),
-    shallowEqual
-  );
+  const { data: user, isLoading: loading } = useGetUsersMeQuery({
+    fields: "name,surname,id,total_exp_pts,image",
+  });
   const { summary, levelInfo } = useSelector(
     (state: GeneralState) => state.ranking,
     shallowEqual
   );
 
-  if (loading || !summary) return <SkeletonHeader />;
+  if (loading || !summary || !user) return <SkeletonHeader />;
   return (
     <HeaderRecap>
       <div className="left-recap">
