360549e31ee5f6d7315f65aac612c862d038e332
Davide Bizzi <davide.bizzi@unguess.io>
2025-10-15 16:14:08 +0200

    feat: refactor BugTypeDescription to conditionally display Bug Parade message

--- src/pages/Preview/Editor/extensions/SlashCommands/nodes/BugTypeDescription/Component/index.tsx
+++ src/pages/Preview/Editor/extensions/SlashCommands/nodes/BugTypeDescription/Component/index.tsx
@@ -1,28 +1,19 @@
 import { Text } from "@appquality/appquality-design-system";
 import { NodeViewWrapper } from "@tiptap/react";
 import { Node as PMNode } from "prosemirror-model";
+import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { useDescription } from "./useDescription";
-import { useGetUsersMeCampaignsByCampaignIdQuery } from "src/services/tryberApi";
-import { useTranslation } from "react-i18next";
 
 export default function MyComponent({ node }: { node: PMNode }) {
   const { t } = useTranslation();
 
   const { id } = useParams<{ id: string }>();
-  const { data: campaign } = useGetUsersMeCampaignsByCampaignIdQuery({
-    campaignId: id,
-  });
   const description = useDescription(id!);
 
   return (
     <NodeViewWrapper className="bug-type-description-node">
       <Text>{description}</Text>
-      {campaign?.hasBugParade === 1 && (
-        <Text>
-          {t("_PAGE_PREVIEW__BUG_PARADE_ACTIVE", "The Bug Parade is active")}
-        </Text>
-      )}
     </NodeViewWrapper>
   );
 }
